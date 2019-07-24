8264ba9de93bab0efe73b957522240efa1537887
Guy Thouret <guy@minds.com>
2019-07-24 14:20:08 +0100

    (fix) Fix warning in Spec 'Cannot declare class MongoDB\BSON\UTCDateTime, because the name is already in use' - #588

--- Spec/bootstrap.php
+++ Spec/bootstrap.php
@@ -306,9 +306,12 @@ if (!class_exists('Cassandra')) {
     class_alias('Mock', 'Cassandra\Uuid');
     class_alias('Mock', 'Cassandra\Timeuuid');
     class_alias('Mock', 'Cassandra\Boolean');
-    class_alias('Mock', 'MongoDB\BSON\UTCDateTime');
     class_alias('Mock', 'Cassandra\RetryPolicy\Logging');
     class_alias('Mock', 'Cassandra\RetryPolicy\DowngradingConsistency');
+}
+
+if (!class_exists('MongoDB\BSON\UTCDateTime')) {
+    class_alias('Mock', 'MongoDB\BSON\UTCDateTime');
 }
 
 Minds\Core\Di\Di::_()->bind('Database\Cassandra\Cql', function($di) {
