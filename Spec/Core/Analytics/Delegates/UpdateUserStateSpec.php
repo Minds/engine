fba7d4e72804acd2ae2174c607996eac083c7114
Guy Thouret <guy@minds.com>
2019-08-07 10:04:40 +0100

    (fix) Test regression in UpdateUserState delegate - #588

--- Spec/Core/Analytics/Delegates/UpdateUserStateSpec.php
+++ Spec/Core/Analytics/Delegates/UpdateUserStateSpec.php
@@ -21,23 +21,14 @@ class UpdateUserStateSpec extends ObjectBehavior
 
     public function it_should_update_user_if_activity_state_change(UserState $userState, User $user)
     {
-        $user->getUserState()->shouldBeCalled()->willReturn(UserState::STATE_UNKNOWN);
         $userState->getState()->shouldBeCalled()->willReturn(UserState::STATE_NEW);
         $userState->getReferenceDateMs()->shouldBeCalled()->willReturn(1563753600000);
         $userState->getUserGuid()->shouldBeCalled()->willReturn(1001);
+        $userState->getStateChange()->shouldBeCalled()->willReturn(1);
         $userState->export()->shouldBeCalled()->willReturn([]);
         $user->setUserState(UserState::STATE_NEW)->shouldBeCalled()->willReturn($user);
         $user->setUserStateUpdatedMs(1563753600000)->shouldBeCalled()->willReturn($user);
         $user->save()->shouldBeCalled();
-
-        $this->update();
-    }
-
-    public function it_should_not_update_user_if_no_activity_state_change(UserState $userState, User $user)
-    {
-        $user->getUserState()->shouldBeCalled()->willReturn(UserState::STATE_NEW);
-        $userState->getState()->shouldBeCalled()->willReturn(UserState::STATE_NEW);
-        $user->save()->shouldNotBeCalled();
 
         $this->update();
     }
