e34c30c4548d11b178d63fccf7018d2265d12846
Emiliano Balbuena <edgebal@gmail.com>
2019-09-22 23:22:21 -0300

    (feat): Implement referrals to Pro Domains

--- Controllers/api/v2/pro/channel.php
+++ Controllers/api/v2/pro/channel.php
@@ -12,11 +12,14 @@ use Minds\Core\Pro\Channel\Manager as ChannelManager;
 use Minds\Core\Pro\Manager;
 use Minds\Core\Session;
 use Minds\Entities\User;
+use Minds\Helpers\Campaigns\Referrals;
 use Minds\Interfaces;
 use Minds\Api\Factory;
 
 class channel implements Interfaces\Api
 {
+    public $request;
+
     /**
      * Equivalent to HTTP GET method
      * @param array $pages
@@ -46,8 +49,17 @@ class channel implements Interfaces\Api
                 ]);
 
             default:
+                $proSettings = $manager->get();
+
                 $exportedChannel = $channel->export();
-                $exportedChannel['pro_settings'] = $manager->get();
+                $exportedChannel['pro_settings'] = $proSettings;
+
+                $origin = strtolower($this->request->getServerParams()['HTTP_X_MINDS_ORIGIN'] ?? '');
+                $domain = strtolower($proSettings->getDomain());
+
+                if ($domain === $origin) {
+                    Referrals::register($channel->username);
+                }
 
                 return Factory::response([
                     'channel' => $exportedChannel,
