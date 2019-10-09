7a65572f807624798c41321bc0e98e317e2334bf
Mark Harding <mark@minds.com>
2019-10-09 10:24:01 +0000

    Entity Centric Manager

---
+++ Core/Analytics/EntityCentric/SignupsSynchroniser.php
@@ -0,0 +1,56 @@
+<?php
+namespace Minds\Core\Analytics\EntityCentric;
+
+use Minds\Core\Analytics\Metrics\Signup;
+use DateTime;
+use Exception;
+
+class SignupsSynchroniser
+{
+    /** @var array */
+    private $records = [];
+
+    /** @var Signup */
+    private $signupMetric;
+
+    public function __construct($signupMetric = null)
+    {
+        $this->signupMetric = $signupMetric ?? new Signup;
+    }
+
+    /**
+     * @param int $from
+     * @return self
+     */
+    public function setFrom($from): self
+    {
+        $this->from = $from;
+        return $this;
+    }
+
+    /**
+     * Convert to records
+     * @return iterable
+     */
+    public function toRecords(): iterable
+    {
+        $date = (new DateTime())->setTimestamp($this->from);
+        $now = new DateTime();
+        $days = (int) $date->diff($now)->format('%a');
+
+        foreach ($this->signupMetric->get($days) as $bucket) {
+            error_log($bucket['date']);
+            $record = new EntityCentricRecord();
+            $record->setEntityUrn("urn:metric:global")
+                ->setOwnerGuid((string) 0) // Site is owner
+                ->setTimestamp($bucket['timestamp'])
+                ->setResolution('day')
+                ->incrementSum('signups::total', $bucket['total']);
+            $this->records[] = $record;
+        }
+        
+        foreach ($this->records as $record) {
+            yield $record;
+        }
+    }
+}
