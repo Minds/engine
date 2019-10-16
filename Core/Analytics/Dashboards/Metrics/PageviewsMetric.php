afcb5fd11eef92d99db651dccc4a8bf269c14b94
Mark Harding <mark@marksmusiconline.co.uk>
2019-10-16 16:44:56 +0000

    (fix): ensure the date histogram is for the entire period

--- Core/Analytics/Dashboards/Metrics/PageviewsMetric.php
+++ Core/Analytics/Dashboards/Metrics/PageviewsMetric.php
@@ -139,7 +139,11 @@ class PageviewsMetric extends AbstractMetric
                         'date_histogram' => [
                             'field' => '@timestamp',
                             'interval' =>  $timespan->getInterval(),
-                            'min_doc_count' =>  1,
+                            'min_doc_count' =>  0,
+                            'extended_bounds' => [
+                                'min' => $timespan->getFromTsMs(),
+                                'max' => time() * 1000,
+                            ],
                         ],
                         'aggs' => [
                             '2' => [
