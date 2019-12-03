Minds/engine

commit 24d1a9279c49240c713583474e0e3d70810d31f0

(feat): only validate the jwt if the access_id isn't set to ACCESS_PUBLIC

Core/Media/Thumbnails.php
@@ -15,9 +15,16 @@ class Thumbnails
         $this->config = $config ?: Di::_()->get('Config');
     }
 
-    public function get($guid, $size)
+    /**
+     * @param $entity Entities\Entity|string
+     * @param $size
+     * @return bool|\ElggFile|mixed|string
+     */
+    public function get($entity, $size)
     {
-        $entity = Entities\Factory::build($guid);
+        if (is_string($entity)) {
+            $entity = Entities\Factory::build($entity);
+        }
         if (!$entity || !Core\Security\ACL::_()->read($entity)) {
             return false;
         }

Controllers/api/v1/media/thumbnails.php
@@ -29,17 +29,6 @@ class thumbnails implements Interfaces\Api, Interfaces\ApiIgnorePam
             exit;
         }
 
-        $featuresManager = new FeaturesManager();
-
-        if ($featuresManager->has('cdn-jwt')) {
-            error_log("{$_SERVER['REQUEST_URI']} was hit, and should not have been");
-
-            return Factory::response([
-                'status' => 'error',
-                'message' => 'This endpoint has been deprecated. Please use fs/v1/thumbnail',
-            ]);
-        }
-
         $guid = $pages[0];
 
         Core\Security\ACL::$ignore = true;
@@ -48,13 +37,33 @@ class thumbnails implements Interfaces\Api, Interfaces\ApiIgnorePam
 
         $last_cache = isset($pages[2]) ? $pages[2] : time();
 
+        $entity = Entities\Factory::build($guid);
+
+        if (!$entity) {
+            return Factory::response([
+                'status' => 'error',
+                'message' => 'Entity not found'
+            ]);
+        }
+
+        $featuresManager = new FeaturesManager();
+
+        if ($entity->access_id !== ACCESS_PUBLIC && $featuresManager->has('cdn-jwt')) {
+            error_log("{$_SERVER['REQUEST_URI']} was hit, and should not have been");
+
+            return Factory::response([
+                'status' => 'error',
+                'message' => 'This endpoint has been deprecated. Please use fs/v1/thumbnail',
+            ]);
+        }
+
         $etag = $last_cache . $guid;
         if (isset($_SERVER['HTTP_IF_NONE_MATCH']) && trim($_SERVER['HTTP_IF_NONE_MATCH']) == $etag) {
             header("HTTP/1.1 304 Not Modified");
             exit;
         }
 
-        $thumbnail = Di::_()->get('Media\Thumbnails')->get($guid, $size);
+        $thumbnail = Di::_()->get('Media\Thumbnails')->get($entity, $size);
 
         if ($thumbnail instanceof \ElggFile) {
             $thumbnail->open('read');

Controllers/fs/v1/thumbnail.php
@@ -18,9 +18,22 @@ class thumbnail extends Core\page implements Interfaces\page
             exit;
         }
 
+        Core\Security\ACL::$ignore = true;
+        $guid = $pages[0];
+        $size = isset($pages[1]) ? $pages[1] : null;
+
+        $entity = Entities\Factory::build($guid);
+
+        if (!$entity) {
+            return Factory::response([
+                'status' => 'error',
+                'message' => 'Entity not found'
+            ]);
+        }
+
         $featuresManager = new FeaturesManager;
 
-        if ($featuresManager->has('cdn-jwt')) {
+        if ($entity->access_id !== ACCESS_PUBLIC && $featuresManager->has('cdn-jwt')) {
             $signedUri = new Core\Security\SignedUri();
             $uri = (string) \Zend\Diactoros\ServerRequestFactory::fromGlobals()->getUri();
             if (!$signedUri->confirm($uri)) {
@@ -31,9 +44,8 @@ class thumbnail extends Core\page implements Interfaces\page
         /** @var Core\Media\Thumbnails $mediaThumbnails */
         $mediaThumbnails = Di::_()->get('Media\Thumbnails');
 
-        Core\Security\ACL::$ignore = true;
-        $size = isset($pages[1]) ? $pages[1] : null;
-        $thumbnail = $mediaThumbnails->get($pages[0], $size);
+
+        $thumbnail = $mediaThumbnails->get($entity, $size);
 
         if ($thumbnail instanceof \ElggFile) {
             $thumbnail->open('read');

commit 34ee75c3fc72b0d85bb0dbf430576704596253f1

(fix): regression caused by 24d1a927

Core/Media/Thumbnails.php
@@ -26,9 +26,10 @@ class Thumbnails
      */
     public function get($entity, $size)
     {
-        if (is_string($entity)) {
-            $entity = $this->entitiesBuilder->build($entity);
+        if (is_numeric($entity)) {
+            $entity = $this->entitiesBuilder->single($entity);
         }
+
         if (!$entity || !Core\Security\ACL::_()->read($entity)) {
             return false;
         }