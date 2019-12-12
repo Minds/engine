ccb4db8ec9ee9a984bab305ec646b5fc064a0966
Mark Harding <mark@minds.com>
2019-12-12 11:43:42 +0000

    Additional stripe steps and ability to add phone number

--- Controllers/Cli/Stripe.php
+++ Controllers/Cli/Stripe.php
@@ -67,4 +67,55 @@ class Stripe extends Cli\Controller implements Interfaces\CliControllerInterface
         $intent = $intentManager->get($id);
         var_dump($intent);
     }
+
+    public function fix_connect()
+    {
+        $connectManager = new Core\Payments\Stripe\Connect\Manager();
+        $i = 0;
+        foreach ($connectManager->getList() as $account) {
+            ++$i;
+            echo "\n$i $account->id";
+            var_dump($account->requirements->currently_due);
+        }
+    }
+
+    public function remove_business_type()
+    {
+        $connectManager = new Core\Payments\Stripe\Connect\Manager();
+        $account = $connectManager->getByAccountId($this->getOpt('id'));
+        $connectManager->update($account);
+    }
+
+    public function create_stripe_lookups()
+    {
+        $connectManager = new Core\Payments\Stripe\Connect\Manager();
+        $iterator = new Core\Analytics\Iterators\SignupsOffsetIterator();
+        $iterator->token = $this->getOpt('token');
+        $i = 0;
+        $s = 0;
+        foreach ($iterator as $user) {
+            if (!$user instanceof Entities\User) {
+                continue;
+            }
+            ++$i;
+            var_dump($user->getMerchant());
+            if ($stripeId = $user->getMerchant()['id']) {
+                ++$s;
+            }
+            echo "\n$s/$i $user->guid {$stripeId} ($iterator->token)";
+            if (!$stripeId) {
+                continue;
+            }
+            try {
+                $account = $connectManager->getByAccountId($stripeId);
+                $account->setEmail($user->getEmail());
+                $account->setUrl('https://www.minds.com/' . $user->username);
+                $account->setMetadata([
+                    'guid' => (string) $user->guid,
+                ]);
+                $connectManager->update($account);
+            } catch (\Exception $e) {
+            }
+        }
+    }
 }
--- Spec/Core/Payments/Stripe/Connect/ManagerSpec.php
+++ Spec/Core/Payments/Stripe/Connect/ManagerSpec.php
@@ -64,6 +64,10 @@ class ManagerSpec extends ObjectBehavior
             ->willReturn(null);
         $account->getPersonalIdNumber()
             ->willReturn(null);
+        $account->getEmail()
+            ->willReturn('test@minds.com');
+        $account->getUrl()
+            ->willReturn('https://minds.com/test');
         
         $this->accountInstance->create(Argument::any())
             ->shouldBeCalled()
@@ -99,7 +103,7 @@ class ManagerSpec extends ObjectBehavior
             ->willReturn((object) [
                 'id' => 'acc_123',
                 'country' => 'GB',
-                'legal_entity' => (object) [
+                'individual' => (object) [
                     'first_name' => 'George',
                     'last_name' => 'Harrison',
                     'gender' => null,
@@ -141,6 +145,7 @@ class ManagerSpec extends ObjectBehavior
                         ],
                     ],
                 ],
+                'metadata' => [],
             ]);
 
         $this->balanceInstance->retrieve([ 'stripe_account' => 'acc_123'])
@@ -179,7 +184,7 @@ class ManagerSpec extends ObjectBehavior
             ->willReturn((object) [
                 'id' => 'acc_123',
                 'country' => 'GB',
-                'legal_entity' => (object) [
+                'individual' => (object) [
                     'first_name' => 'George',
                     'last_name' => 'Harrison',
                     'gender' => null,
@@ -220,7 +225,8 @@ class ManagerSpec extends ObjectBehavior
                             'monthly_anchor' => 31,
                         ]
                     ]
-                ]
+                ],
+                'metadata' => [],
             ]);
 
         $this->balanceInstance->retrieve([ 'stripe_account' => 'acc_123'])
