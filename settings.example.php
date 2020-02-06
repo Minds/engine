07b4012dcd94c3cac99f423d33e58a5d44a35e26
Olivia Madrid <oliviajmadrid@gmail.com>
2020-02-06 15:50:05 -0700

    toggle feature flag to true

--- settings.example.php
+++ settings.example.php
@@ -487,7 +487,7 @@ $CONFIG->set('features', [
     'onboarding-december-2019' => true,
     'register_pages-december-2019' => true,
     'modal-pager' => true,
-    'wallet-upgrade' => false
+    'wallet-upgrade' => true
 ]);
 
 $CONFIG->set('email', [
