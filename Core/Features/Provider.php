349064a3e60d19ec8079c39345d96960195624bf
Mark Harding <mark@kramnorth.com>
2020-02-20 12:58:14 +0000

    (chore): feature flags for new activity front#2511

--- Core/Features/Provider.php
+++ Core/Features/Provider.php
@@ -45,6 +45,9 @@ class Provider extends DiProvider
                 'cdn-jwt',
                 'post-scheduler',
                 'navigation',
+                'activity-v2--single-page',
+                'activity-v2--feeds',
+                'activity-v2--boosts',
             ];
         });
 
