55c805eda9094316f6a1aeb16273b491d5319781
Mark Harding <mark@kramnorth.com>
2020-03-10 14:37:39 +0000

    Revert "Merge branch 'fix/wire-acl-1379' into 'master'"
    This reverts commit 171902c71da6c304840c9520e024ca27b9ff0b08, reversing
    changes made to 5b2bff6d90bdb2e33f267f596a402aa6081cb021.

--- Core/Security/ACL.php
+++ Core/Security/ACL.php
@@ -13,7 +13,6 @@ use Minds\Entities\RepositoryEntity;
 use Minds\Entities\User;
 use Minds\Exceptions\StopEventException;
 use Minds\Helpers\Flags;
-use Minds\Helpers\MagicAttributes;
 
 class ACL
 {
@@ -197,8 +196,7 @@ class ACL
         /**
          * Check if its the same entity (is user)
          */
-        if ((isset($entity->guid) && $entity->guid == $user->guid) ||
-            MagicAttributes::getterExists($entity, 'getGuid') && $entity->getGuid() == $user->guid) {
+        if ($entity->guid == $user->guid) {
             return true;
         }
 
@@ -212,7 +210,6 @@ class ACL
         /**
          * Allow plugins to extend the ACL check
          */
-        $type = property_exists($entity, 'type') ? $entity->type : 'all';
         if (Core\Events\Dispatcher::trigger('acl:write', $entity->type, ['entity'=>$entity, 'user'=>$user], false) === true) {
             return true;
         }
