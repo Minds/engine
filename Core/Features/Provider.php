aeb9bf4432488538d3069ec0deda3db001a635c6
Olivia Madrid <oliviajmadrid@gmail.com>
2020-03-12 10:03:20 -0600

    Add channels FF to provider

--- Core/Features/Provider.php
+++ Core/Features/Provider.php
@@ -51,6 +51,7 @@ class Provider extends DiProvider
                 'activity-v2--feeds',
                 'activity-v2--boosts',
                 'settings',
+                'channels',
             ];
         });
 
