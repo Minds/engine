7c69622793739fc46f3c76ec9cd5987c1d23e7e9
Marcelo Rivera <malrogf@gmail.com>
2020-03-24 16:03:09 -0300

    (fix): spec tests

--- Spec/Core/Search/Mappings/UserMappingSpec.php
+++ Spec/Core/Search/Mappings/UserMappingSpec.php
@@ -39,6 +39,7 @@ class UserMappingSpec extends ObjectBehavior
         $user->get('rating')->willReturn(1);
         $user->get('moderator_guid')->willReturn('123');
         $user->get('time_moderated')->willReturn($now);
+        $user->get('banned')->willReturn('no');
         $user->isBanned()->willReturn(false);
         $user->getEmailConfirmedAt()
             ->shouldBeCalled()
@@ -77,6 +78,7 @@ class UserMappingSpec extends ObjectBehavior
                 'username' => 'phpspec',
                 'briefdescription' => 'PHPSpec Brief Description #invalidhashtag',
                 'email_confirmed_at' =>  $now * 1000,
+                'banned' => 'no',
                 '@timestamp' => $now * 1000,
                 'taxonomy' => 'user',
                 'public' => true,
@@ -88,47 +90,6 @@ class UserMappingSpec extends ObjectBehavior
             ]);
     }
 
-    public function it_should_throw_exception_if_banned(User $user)
-    {
-        $now = time();
-
-        $user->get('interactions')->willReturn(42);
-        $user->get('guid')->willReturn(1000);
-        $user->get('type')->willReturn('user');
-        $user->get('subtype')->willReturn('');
-        $user->get('time_created')->willReturn($now);
-        $user->get('email_confirmed_at')->willReturn($now);
-        $user->get('access_id')->willReturn(2);
-        $user->get('owner_guid')->willReturn(false);
-        $user->get('container_guid')->willReturn(1000);
-        $user->get('mature')->willReturn(false);
-        $user->get('is_mature')->willReturn(false);
-        $user->get('message')->willReturn('PHPSpec Message #test #hashtag');
-        $user->get('name')->willReturn('PHPSpec Name');
-        $user->get('title')->willReturn('PHPSpec Title');
-        $user->get('blurb')->willReturn('PHPSpec Blurb');
-        $user->get('description')->willReturn('PHPSpec Description');
-        $user->get('paywall')->willReturn(false);
-        $user->get('username')->willReturn('phpspec');
-        $user->get('briefdescription')->willReturn('PHPSpec Brief Description #invalidhashtag');
-        $user->get('rating')->willReturn(1);
-        $user->isBanned()->willReturn(true);
-
-        $user->getNsfw()->willReturn([ 1 ]);
-        $user->isMature()->willReturn(true);
-        $user->getMatureContent()->willReturn(false);
-        $user->getGroupMembership()->willReturn([ 2000 ]);
-        $user->get('moderator_guid')->willReturn(null);
-        $user->get('time_moderated')->willReturn(null);
-        $this
-            ->setEntity($user)
-            ->shouldThrow('Minds\Exceptions\BannedException')
-            ->duringMap([
-                'passedValue' => 'PHPSpec',
-                'guid' => '4999-will-disappear'
-            ]);
-    }
-
     public function it_should_suggest_map(
         User $user
     ) {
@@ -136,6 +97,7 @@ class UserMappingSpec extends ObjectBehavior
         $user->get('name')->willReturn('testing framework');
         $user->get('featured_id')->willReturn(12000);
         $user->isAdmin()->willReturn(true);
+        $user->isBanned()->willReturn(false);
 
         $this
             ->setEntity($user)
@@ -152,6 +114,25 @@ class UserMappingSpec extends ObjectBehavior
                 'weight' => 152
             ]);
     }
+
+    public function it_should_return_an_empty_suggest_map_if_banned(
+        User $user
+    ) {
+        $user->get('username')->willReturn('phpspec');
+        $user->get('name')->willReturn('testing framework');
+        $user->get('featured_id')->willReturn(12000);
+        $user->isAdmin()->willReturn(true);
+        $user->isBanned()->willReturn(true);
+
+        $this
+            ->setEntity($user)
+            ->suggestMap([
+                'passedValue' => 'PHPSpec',
+                'input' => 'phpspec-will-disappear'
+            ])
+            ->shouldReturn([]);
+    }
+
     public function it_should_suggest_map_permutating_camelcase_name(
         User $user
     ) {
@@ -159,6 +140,7 @@ class UserMappingSpec extends ObjectBehavior
         $user->get('name')->willReturn('TestingFramework');
         $user->get('featured_id')->willReturn(12000);
         $user->isAdmin()->willReturn(true);
+        $user->isBanned()->willReturn(false);
 
         $this
             ->setEntity($user)
