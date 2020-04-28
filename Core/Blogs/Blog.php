4b1c91f7fb89b27c07ac2d7dd104fc8ae686b4dd
Ben Hayward <ben@minds.com>
2020-04-28 17:19:18 +0000

    Epic/ckeditor5 blogs (engine)

--- Core/Blogs/Blog.php
+++ Core/Blogs/Blog.php
@@ -616,7 +616,9 @@ class Blog extends RepositoryEntity
         $output = [];
 
         // Sanitize body
-        $output['body'] = (new XSS())->clean($export['body']);
+        $output['body'] = (new XSS())
+            ->setAllowed(['<pre>', '<code>', '<figure>', '<figcaption>', '<table>', '<tbody>', '<tr>', '<td>'])
+            ->clean($export['body']);
 
         // Legacy
         $output['ownerObj'] = $this->getOwnerObj();
