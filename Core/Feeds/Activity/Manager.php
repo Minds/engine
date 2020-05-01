5d4fa9df80a908a5cf451d51b56e4e4cd0096696
Marcelo Rivera <malrogf@gmail.com>
2020-05-01 15:48:00 +0000

    (feat): refactor classes to use a Manager (feat): refactor so a User can register more than one channel (feat): /videos POST API (fix): remove /video/import API

--- Core/Feeds/Activity/Manager.php
+++ Core/Feeds/Activity/Manager.php
@@ -78,7 +78,7 @@ class Manager
         $activity->setEdited(true);
 
         $activity->indexes = ["activity:$activity->owner_guid:edits"]; //don't re-index on edit
-        
+
         $this->translationsDelegate->onUpdate($activity);
 
         if ($activityMutation->hasMutated('timeCreated')) {
@@ -123,15 +123,17 @@ class Manager
     public function createFromEntity($entity): Activity
     {
         $activity = new Activity();
-        $activity->setTimeCreated(time());
-        $activity->setTimeSent(time());
+        $activity->setTimeCreated($entity->getTimeCreated() ?: time());
+        $activity->setTimeSent($entity->getTimeCreated() ?: time());
         $activity->setTitle($entity->title);
         $activity->setMessage($entity->description);
         $activity->setFromEntity($entity);
+        $activity->owner_guid = $entity->owner_guid;
+        $activity->container_guid = $entity->container_guid;
         $activity->access_id = $entity->access_id;
 
         if ($entity->type === 'object' && in_array($entity->subtype, ['image', 'video'], true)) {
-            $activity->setCustom($entity->getActivityParameters());
+            $activity->setCustom(...$entity->getActivityParameters());
         }
 
         return $activity;
