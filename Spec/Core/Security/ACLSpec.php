7253cd7b361ed37fa94237feb7686df6efb7502a
Mark Harding <mark@kramnorth.com>
2020-05-15 09:17:07 +0100

    (fix): spec tests

--- Spec/Core/Security/ACLSpec.php
+++ Spec/Core/Security/ACLSpec.php
@@ -8,17 +8,22 @@ use Minds\Core;
 use Minds\Entities\User;
 use Minds\Entities\Entity;
 use Minds\Entities\MindsObject;
+use Minds\Core\EntitiesBuilder;
 
 class ACLSpec extends ObjectBehavior
 {
     /** @var Core\Security\RateLimits\Manager */
     private $rateLimits;
 
-    public function let(Core\Security\RateLimits\Manager $rateLimits)
+    /** @var EntitiesBuilder */
+    private $entitiesBuilder;
+
+    public function let(Core\Security\RateLimits\Manager $rateLimits, EntitiesBuilder $entitiesBuilder)
     {
         $this->rateLimits = $rateLimits;
+        $this->entitiesBuilder = $entitiesBuilder;
 
-        $this->beConstructedWith($rateLimits);
+        $this->beConstructedWith($rateLimits, $entitiesBuilder);
     }
 
     public function mock_session($on = true)
@@ -38,8 +43,22 @@ class ACLSpec extends ObjectBehavior
         $this->shouldHaveType('Minds\Core\Security\ACL');
     }
 
-    public function it_should_allow_read_of_public_entities(Entity $entity)
+    public function it_should_allow_read_of_public_entities(Entity $entity, User $user)
     {
+        $user->get('type')->willReturn('user');
+        $user->get('guid')->willReturn(123);
+        $user->get('access_id')->willReturn(2);
+        $user->get('owner_guid')->willReturn(0);
+        $user->get('container_guid')->willReturn(0);
+        $user->getSpam()->willReturn(false);
+        $user->getDeleted()->willReturn(false);
+        $user->isBanned()->willReturn(false);
+        $user->isEnabled()->willReturn(true);
+        
+        $this->entitiesBuilder->single(123, [ 'cache' => true, 'cacheTtl' => 604800 ])
+            ->shouldBeCalled()
+            ->willReturn($user);
+
         $entity->get('access_id')->willReturn(2);
         $entity->get('container_guid')->willReturn(123);
         $entity->get('owner_guid')->willReturn(123);
@@ -47,8 +66,47 @@ class ACLSpec extends ObjectBehavior
         $this->read($entity)->shouldReturn(true);
     }
 
-    public function it_should_not_allow_read_of_private_entities(Entity $entity)
+    public function it_should_not_allow_posts_from_bad_users(Entity $entity, User $user)
     {
+        $user->get('type')->willReturn('user');
+        $user->get('guid')->willReturn(123);
+        $user->get('access_id')->willReturn(2);
+        $user->get('owner_guid')->willReturn(0);
+        $user->get('container_guid')->willReturn(0);
+        $user->get('username')->willReturn('banneduser');
+        $user->getSpam()->willReturn(false);
+        $user->getDeleted()->willReturn(false);
+        $user->isBanned()->willReturn(true);
+        $user->isEnabled()->willReturn(true);
+        
+        $this->entitiesBuilder->single(123, [ 'cache' => true, 'cacheTtl' => 604800 ])
+            ->shouldBeCalled()
+            ->willReturn($user);
+
+        $entity->get('guid')->willReturn(1);
+        $entity->get('access_id')->willReturn(2);
+        $entity->get('container_guid')->willReturn(123);
+        $entity->get('owner_guid')->willReturn(123);
+        $entity->get('type')->willReturn('activity');
+        $this->read($entity)->shouldReturn(false);
+    }
+
+    public function it_should_not_allow_read_of_private_entities(Entity $entity, User $user)
+    {
+        $user->get('type')->willReturn('user');
+        $user->get('guid')->willReturn(123);
+        $user->get('access_id')->willReturn(2);
+        $user->get('owner_guid')->willReturn(0);
+        $user->get('container_guid')->willReturn(0);
+        $user->getSpam()->willReturn(false);
+        $user->getDeleted()->willReturn(false);
+        $user->isBanned()->willReturn(false);
+        $user->isEnabled()->willReturn(true);
+        
+        $this->entitiesBuilder->single(123, [ 'cache' => true, 'cacheTtl' => 604800 ])
+            ->shouldBeCalled()
+            ->willReturn($user);
+    
         $entity->getType()->willReturn('specy');
         $entity->get('access_id')->willReturn(0);
         $entity->get('owner_guid')->willReturn(123);
