b205a30bbe4047ca2134730eccc64733c6290d17
Olivia Madrid <oliviajmadrid@gmail.com>
2020-05-20 16:34:05 -0600

    don't send old ban emails anymore

--- Core/Reports/Events.php
+++ Core/Reports/Events.php
@@ -20,26 +20,8 @@ class Events
         Di::_()->get('EventsDispatcher')->register('ban', 'user', function ($event) {
             $config = Di::_()->get('Config');
             $user = $event->getParameters();
-            //send ban email
-            $template = new Core\Email\Template();
-            $template
-                ->setTemplate()
-                ->setBody('banned.tpl')
-                ->set('username', $user->username)
-                ->set('email', $user->getEmail())
-                ->set('reason', $this->getBanReasons($user->ban_reason))
-                ->set('user', $user);
-
-            $message = new Core\Email\Message();
-            $message->setTo($user)
-                ->setMessageId(implode('-', [$user->guid, sha1($user->getEmail()), sha1('register-' . time())]))
-                ->setSubject("You are banned from Minds.")
-                ->setReplyTo($config->get('contact_details')['email'], $config->get('contact_details')['name'])
-                ->setHtml($template);
-            Di::_()->get('Mailer')->queue($message);
 
             // Record metric
-
             $event = new Event();
             $event->setType('action')
                 ->setProduct('platform')
