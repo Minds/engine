26072794c8fddec65b476ad8807db0920345d81d
Mark Harding <mark@kramnorth.com>
2020-05-21 06:36:15 +0100

    (chore); remove legacy is installed step

--- Core/Minds.php
+++ Core/Minds.php
@@ -135,8 +135,6 @@ class Minds extends base
      */
     public function start()
     {
-        $this->checkInstalled();
-
         $this->loadConfigs();
         $this->loadLegacy();
         $this->loadEvents();
@@ -258,24 +256,6 @@ class Minds extends base
         }
 
         return false;
-    }
-
-    /**
-     * Check if Minds is installed, if not redirect to install script.
-     */
-    public function checkInstalled()
-    {
-        /*
-         * If we are a multisite, we get the install status from the multisite settings
-         */
-        if ($this->detectMultisite()) {
-            //we do this on db load.. not here
-        } else {
-            if (!file_exists(__MINDS_ROOT__.'/settings.php') && !defined('__MINDS_INSTALLING__')) {
-                header('Location: install.php');
-                exit;
-            }
-        }
     }
 
     /**
