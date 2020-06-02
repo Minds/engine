21ce428d05460307024978fca260ab30765de54c
Mark Harding <mark@kramnorth.com>
2020-06-02 14:09:04 +0100

    (fix): groups require numbers to be exportedg

--- Entities/Group.php
+++ Entities/Group.php
@@ -877,6 +877,12 @@ class Group extends NormalizedEntity
     {
         $export = parent::export($keys);
 
+        foreach ($export as $key => $value) {
+            if (is_numeric($value)) {
+                $export[$key] = (int) $value;
+            }
+        }
+
         // Compatibility keys
         $export['owner_guid'] = $this->getOwnerObj()->guid;
         //$export['activity:count'] = $this->getActivityCount();
