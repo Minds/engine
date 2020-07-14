6cc338db2c245895aaed248944d3063e32f1e373
Mark Harding <mark@marksmusiconline.co.uk>
2020-07-14 12:39:28 +0000

    (feat): provides and overview of earnings - #1619

---
+++ Core/Monetization/EarningsOverview/EarningsGroupModel.php
@@ -0,0 +1,50 @@
+<?php
+namespace Minds\Core\Monetization\EarningsOverview;
+
+use Minds\Traits\MagicAttributes;
+
+class EarningsGroupModel
+{
+    use MagicAttributes;
+
+    /** @var string */
+    private $id;
+
+    /** @var array */
+    private $items = [];
+
+    /** @var string */
+    private $currency = 'usd';
+
+    public function getSum(): int
+    {
+        return array_reduce(
+            array_map(function ($item) {
+                return $item->getAmountCents();
+            }, $this->items),
+            function ($carry, $item) {
+                $carry += (int) $item;
+                return $carry;
+            },
+            0
+        );
+    }
+    
+    /**
+     * Export the overview
+     * @return array
+     */
+    public function export(): array
+    {
+        $amountCents = $this->getSum();
+        return [
+            'id' => $this->id,
+            'amount_cents' => $amountCents,
+            'amount_usd' => $amountCents / 100,
+            'currency' => $this->currency,
+            'items' => array_map(function ($item) {
+                return $item->export();
+            }, $this->items),
+        ];
+    }
+}
