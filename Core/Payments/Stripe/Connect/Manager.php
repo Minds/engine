b4f9bec8c7f4d46454473f070d081bdb8067af8f
Mark Harding <mark@marksmusiconline.co.uk>
2020-07-22 09:55:36 +0000

    (feat): add total paid out field to stripe connect object - #1628

--- Core/Payments/Stripe/Connect/Manager.php
+++ Core/Payments/Stripe/Connect/Manager.php
@@ -8,6 +8,7 @@ use Minds\Core\Payments\Stripe\Currencies;
 use Minds\Core\Payments\Stripe\Instances\AccountInstance;
 use Minds\Core\Payments\Stripe\Instances\BalanceInstance;
 use Minds\Core\Payments\Stripe\Instances\FileInstance;
+use Minds\Core\Payments\Stripe\Transactions;
 use Stripe;
 use Minds\Entities\User;
 
@@ -27,19 +28,24 @@ class Manager
 
     /** @var FileInstance $fileInstance */
     private $fileInstance;
+    
+    /** @var Transactions\Manager */
+    private $transactionsManager;
 
     public function __construct(
         Save $save = null,
         NotificationDelegate $notificationDelegate = null,
         AccountInstance $accountInstance = null,
         BalanceInstance $balanceInstance = null,
-        FileInstance $fileInstance = null
+        FileInstance $fileInstance = null,
+        Transactions\Manager $transactionsManager = null
     ) {
         $this->save = $save ?: new Save();
         $this->notificationDelegate = $notificationDelegate ?: new NotificationDelegate();
         $this->accountInstance = $accountInstance ?: new AccountInstance();
         $this->balanceInstance = $balanceInstance ?: new BalanceInstance();
         $this->fileInstance = $fileInstance ?: new FileInstance();
+        $this->transactionsManager = $transactionsManager ?? new Transactions\Manager();
     }
 
     /**
@@ -340,6 +346,7 @@ class Manager
 
             $account->setTotalBalance($this->getBalanceById($result->id, 'available'));
             $account->setPendingBalance($this->getBalanceById($result->id, 'pending'));
+            $account->setTotalPaidOut($this->getTotalPaidOut($account));
 
             return $account;
         } catch (Stripe\Error\Permission $e) {
@@ -375,6 +382,16 @@ class Manager
         $balance->setAmount($stripeBalance->$type[0]->amount)
             ->setCurrency($stripeBalance->$type[0]->currency);
         return $balance;
+    }
+
+    protected function getTotalPaidOut(Account $account): Balance
+    {
+        $total = new Balance();
+        foreach ($this->transactionsManager->setAccount($account)->getPayouts() as $payout) {
+            $total->setCurrency($payout->getCurrency());
+            $total->setAmount($total->getAmount() + ($payout->getGross() * -1));
+        }
+        return $total;
     }
 
     /**
