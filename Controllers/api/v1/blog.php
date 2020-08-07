26c5fddade578ec5b22188f8eca7c12086f590d9
Mark Harding <mark@marksmusiconline.co.uk>
2020-08-07 12:51:18 +0000

    (feat): require a minimum token balance for users who wish to post links

--- Controllers/api/v1/blog.php
+++ Controllers/api/v1/blog.php
@@ -17,6 +17,7 @@ use Minds\Core\Router\Exceptions\UnverifiedEmailException;
 use Minds\Helpers;
 use Minds\Interfaces;
 use Minds\Core\Blogs\Delegates\CreateActivity;
+use Minds\Entities\User;
 
 class blog implements Interfaces\Api
 {
@@ -198,10 +199,17 @@ class blog implements Interfaces\Api
             ]);
         }
 
-        if (!$captcha->verifyFromClientJson($_POST['captcha'] ?? '')) {
+        /*if (!$captcha->verifyFromClientJson($_POST['captcha'] ?? '')) {
             return Factory::response([
                 'status' => 'error',
                 'message' => 'Please ensure that the captcha you entered is correct',
+            ]);
+        }*/
+
+        if (!$this->checkBalance(Core\Session::getLoggedInUser()) && preg_match('/(\b(https?|ftp|file):\/\/[^\s\]]+)/im', $_POST['description'] ?? '')) {
+            return Factory::response([
+                'status' => 'error',
+                'message' => 'You must have tokens in your OffChain or OnChain wallets to make a blog with hyperlinks',
             ]);
         }
 
@@ -419,5 +427,19 @@ class blog implements Interfaces\Api
         }
 
         return Factory::response([]);
+    }
+
+    /**
+     * Checks the balance
+     * @param User $user
+     * @return bool
+     */
+    private function checkBalance(User $user): bool
+    {
+        return Di::_()->get('Blockchain\Wallets\Balance')
+            ->setUser($user)
+            ->get()
+            ->div(10 ** 18)
+            ->toDouble() > 0;
     }
 }
