4f1eb94992cecd7a981ff6f1d22ef5ff11d8738a
Ben Hayward <ben@minds.com>
2020-08-11 08:15:08 +0000

    Fix/can edit sentry error 3603

--- Controllers/api/v1/comments.php
+++ Controllers/api/v1/comments.php
@@ -101,10 +101,12 @@ class comments implements Interfaces\Api
           case "update":
             $comment = $manager->getByLuid($pages[1]);
 
-            $canEdit = $comment->canEdit();
+            if ($comment) {
+                $canEdit = $comment->canEdit();
 
-            if ($canEdit && $comment->getOwnerGuid() != Core\Session::getLoggedInUserGuid()) {
-                $canEdit = false;
+                if ($canEdit && $comment->getOwnerGuid() != Core\Session::getLoggedInUserGuid()) {
+                    $canEdit = false;
+                }
             }
 
             if (!$comment || !$canEdit) {
