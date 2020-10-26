7e82e873df7ab9d397f60b3344423f074d43df28
Mark Harding <mark@kramnorth.com>
2020-10-26 16:33:31 +0000

    (fix): downgrade to composer 1.10.16

--- tools/setup.sh
+++ tools/setup.sh
@@ -15,11 +15,11 @@ rm -rf ../vendor
 # Hash update information - https://getcomposer.org/download/
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('sha384', 'composer-setup.php') === 'c31c1e292ad7be5f49291169c0ac8f683499edddcfd4e42232982d0fd193004208a58ff6f353fde0012d35fdd72bc394') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-php composer-setup.php
+php composer-setup.php --version=1.10.16
 php -r "unlink('composer-setup.php');"
 
 # Optimise for package install speed
-# php composer.phar -n global require -n "hirak/prestissimo"
+php composer.phar -n global require -n "hirak/prestissimo"
 
 # Grab dependencies
 php composer.phar install $INSTALLOPTS --ignore-platform-reqs
