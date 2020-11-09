d5d5aa837e1d62a9a7fdb41c122bae34fee8a3b2
Mark Harding <mark@kramnorth.com>
2020-11-09 11:29:12 +0000

    (feat): support .00 wire payments so we can charge stripe legacy subscriptions - #1768

--- Core/Wire/Delegates/UpgradesDelegate.php
+++ Core/Wire/Delegates/UpgradesDelegate.php
@@ -1,7 +1,9 @@
 <?php
+
 /**
  * Upgrades Delegate
  */
+
 namespace Minds\Core\Wire\Delegates;
 
 use Minds\Core\Config;
@@ -36,7 +38,7 @@ class UpgradesDelegate
     public function onWire($wire, $receiver_address): Wire
     {
         switch ($wire->getReceiver()->guid) {
-            case $this->config->get('blockchain')['contracts']['wire']['plus_guid']:
+            case $this->config->get('plus')['handler']:
                 return $this->onPlusUpgrade($wire, $receiver_address);
                 break;
             case $this->config->get('pro')['handler']:
@@ -46,22 +48,14 @@ class UpgradesDelegate
         return $wire; // Not expected
     }
 
+    /**
+     * If plus wire detected
+     * @param Wire $wire
+     * @param string $receiver_address - (eth address or guid)
+     * @return Wire
+     */
     private function onPlusUpgrade($wire, $receiver_address): Wire
     {
-        /*if (
-            !(
-                $receiver_address == 'offchain'
-                || $receiver_address == $this->config->get('blockchain')['contracts']['wire']['plus_address']
-            )
-        ) {
-            return $wire; //not offchain or potential onchain fraud
-        }
-
-        // 20 tokens
-        if ($wire->getAmount() != "20000000000000000000") {
-            return $wire; //incorrect wire amount sent
-        }*/
-
         //set the plus period for this user
         $user = $wire->getSender();
 
@@ -77,7 +71,7 @@ class UpgradesDelegate
         $days = 30;
         $monthly = $this->config->get('upgrades')['plus']['monthly'];
         $yearly = $this->config->get('upgrades')['plus']['yearly'];
-        
+
         switch ($wire->getMethod()) {
             case 'tokens':
                 if ($monthly['tokens'] == $wire->getAmount() / (10 ** 18)) {
@@ -89,6 +83,12 @@ class UpgradesDelegate
                 }
                 break;
             case 'usd':
+                if ($user->plus_expires > strtotime('40 days ago') && $wire->getAmount() == 500 && php_sapi_name() === 'cli') {
+                    // If user has had Minds+ before, in the last billing period, and we are running via the CLI
+                    // treat as legacy subscription customer
+                    $days = 32;
+                    break;
+                }
                 // Users who have never had Minds+ before get a 7 day trial
                 // we still create the subscription, but do no charge for 7 days
                 if ($wire->isTrial()) {
@@ -110,7 +110,6 @@ class UpgradesDelegate
         $user->setPlusExpires($expires);
         $user->save();
 
-        //$wire->setSender($user);
         return $wire;
     }
 
@@ -136,13 +135,13 @@ class UpgradesDelegate
         switch ($wire->getMethod()) {
             case 'tokens':
                 error_log($wire->getAmount());
-                  if ($monthly['tokens'] == $wire->getAmount() / (10 ** 18)) {
-                      $days = 32;
-                  } elseif ($yearly['tokens'] == $wire->getAmount() / (10 ** 18)) {
-                      $days = 367;
-                  } else {
-                      return $wire;
-                  }
+                if ($monthly['tokens'] == $wire->getAmount() / (10 ** 18)) {
+                    $days = 32;
+                } elseif ($yearly['tokens'] == $wire->getAmount() / (10 ** 18)) {
+                    $days = 367;
+                } else {
+                    return $wire;
+                }
                 break;
             case 'usd':
                 if ($monthly['usd'] == $wire->getAmount() / 100) {
