fba16c4b80ee5e74fe2e8739e25598a98671a160
Mark Harding <mark@minds.com>
2020-11-16 10:21:08 +0000

    (refactor): #1454 - Reminds overhaul. Quoted posts and merged posts

--- Core/Feeds/Activity/Manager.php
+++ Core/Feeds/Activity/Manager.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Manager
  * @author edgebal
@@ -7,10 +8,16 @@
 namespace Minds\Core\Feeds\Activity;
 
 use Minds\Entities\Activity;
+use Minds\Entities\Image;
+use Minds\Entities\Video;
 use Minds\Common\EntityMutation;
 use Minds\Core\Entities\Actions\Save;
+use Minds\Core\Entities\Actions\Delete;
 use Minds\Core\Entities\PropagateProperties;
+use Minds\Core\EntitiesBuilder;
+use Minds\Core\Di\Di;
 use Minds\Core\Session;
+use Minds\Common\Urn;
 
 class Manager
 {
@@ -32,11 +39,23 @@ class Manager
     /** @var Delegates\PaywallDelegate */
     private $paywallDelegate;
 
+    /** @var Delegates\MetricsDelegate */
+    private $metricsDelegate;
+
+    /** @var Delegates\NotificationsDelegate */
+    private $notificationsDelegate;
+
     /** @var Save */
     private $save;
 
+    /** @var Delete */
+    private $delete;
+
     /** @var PropagateProperties */
     private $propagateProperties;
+
+    /** @var EntitiesBuilder */
+    private $entitiesBuilder;
 
     public function __construct(
         $foreignEntityDelegate = null,
@@ -44,18 +63,81 @@ class Manager
         $attachmentDelegate = null,
         $timeCreatedDelegate = null,
         $save = null,
+        $delete = null,
         $propagateProperties = null,
         $videoPosterDelegate = null,
-        $paywallDelegate = null
+        $paywallDelegate = null,
+        $metricsDelegate = null,
+        $notificationsDelegate = null,
+        $entitiesBulder = null
     ) {
         $this->foreignEntityDelegate = $foreignEntityDelegate ?? new Delegates\ForeignEntityDelegate();
         $this->translationsDelegate = $translationsDelegate ?? new Delegates\TranslationsDelegate();
         $this->attachmentDelegate = $attachmentDelegate ?? new Delegates\AttachmentDelegate();
         $this->timeCreatedDelegate = $timeCreatedDelegate ?? new Delegates\TimeCreatedDelegate();
         $this->save = $save ?? new Save();
+        $this->delete = $delete ?? new Delete();
         $this->propagateProperties = $propagateProperties ?? new PropagateProperties();
         $this->videoPosterDelegate = $videoPosterDelegate ?? new Delegates\VideoPosterDelegate();
         $this->paywallDelegate = $paywallDelegate ?? new Delegates\PaywallDelegate();
+        $this->metricsDelegate = $metricsDelegate ?? new Delegates\MetricsDelegate();
+        $this->notificationsDelegate = $notificationsDelegate ?? new Delegates\NotificationsDelegate();
+        $this->entitiesBuilder = $entitiesBuilder ?? Di::_()->get('EntitiesBuilder');
+    }
+
+    /**
+     * Add an activity
+     * @param Activity $activity
+     * @return bool
+     */
+    public function add(Activity $activity): bool
+    {
+        // Ensure reminds & quoted posts inherit the NSFW settings
+        // NOTE: this is not fool proof. If the original entity changes, we still
+        // need to create a feature that will propogate these settings to its child derivatives.
+        if ($activity->isRemind() || $activity->isQuotedPost()) {
+            $remind = $activity->getRemind();
+            $activity->setNsfw(array_merge($remind->getNsfw(), $activity->getNsfw()));
+        }
+
+        $success = $this->save
+            ->setEntity($activity)
+            ->save();
+
+        if ($success) {
+            $this->metricsDelegate->onAdd($activity);
+            $this->notificationsDelegate->onAdd($activity);
+        }
+
+        return $success;
+    }
+
+    /**
+     * Delete activity
+     * @param Activity $activity
+     * @return bool
+     */
+    public function delete(Activity $activity): bool
+    {
+        return $this->delete->setEntity($activity)->delete();
+    }
+
+    /**
+     * Get by urn
+     * @param string $urn
+     * @return Activity
+     */
+    public function getByUrn(string $urn): ?Activity
+    {
+        $urn = new Urn($urn);
+        $guid = $urn->getNss();
+        $entity = $this->entitiesBuilder->single($guid);
+
+        if (!$entity instanceof Activity) {
+            return null; // TODO throw invalid type exception
+        }
+
+        return $entity;
     }
 
     /**
@@ -140,13 +222,31 @@ class Manager
         $activity->owner_guid = $entity->owner_guid;
         $activity->container_guid = $entity->container_guid;
         $activity->access_id = $entity->access_id;
-        $activity->setTags($entity->tags);
+        $activity->setTags($entity->tags ?: []);
 
         if ($entity->type === 'object' && in_array($entity->subtype, ['image', 'video'], true)) {
+            /** @var Video|Image */
+            $entity = $media = $entity; // Helper for static analysis
             $activity->setCustom(...$entity->getActivityParameters());
             $activity->setPayWall($entity->getFlag('paywall'));
         }
 
+        if ($entity->subtype === 'blog') {
+            /** @var \Minds\Core\Blogs\Blog */
+            $entity = $blog = $entity; // Helper for static analysis
+            $activity->setTitle($entity->getTitle())
+                ->setBlurb(strip_tags($entity->getBody()))
+                ->setURL($entity->getURL())
+                ->setThumbnail($entity->getIconUrl());
+        }
+
         return $activity;
+    }
+
+    /**
+     *
+     */
+    public function getByGuid(string $guid): ?Activity
+    {
     }
 }
