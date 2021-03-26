2dbe51fe95c14e7cdae221c1cd40a892ebfbd8d5
Olivia Madrid <olivia@minds.com>
2021-03-26 15:18:35 +0000

    TOTP recovery code flow

--- Spec/Core/Security/TOTP/ManagerSpec.php
+++ Spec/Core/Security/TOTP/ManagerSpec.php
@@ -6,10 +6,13 @@ use Minds\Core\Security\TOTP\Manager;
 use Minds\Core\Security\TOTP\Repository;
 use Minds\Core\Security\TOTP\TOTPSecret;
 use Minds\Core\Security\TOTP\TOTPSecretQueryOpts;
+use Minds\Core\Security\TOTP\Delegates\EmailDelegate;
+use Minds\Core\Security\Password;
 use Minds\Common\Repository\Response;
 use Minds\Entities\User;
 use Minds\Exceptions\UserErrorException;
 use Exception;
+use Minds\Core\EntitiesBuilder;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
@@ -18,10 +21,23 @@ class ManagerSpec extends ObjectBehavior
     /** @var Repository */
     protected $repository;
 
-    public function let(Repository $repository)
+    /** @var Password */
+    protected $password;
+
+    /** @var EntitiesBuilder */
+    protected $entitiesBuilder;
+
+    /** @var Delegates\EmailDelegate */
+    private $emailDelegate;
+
+    public function let(Repository $repository, Password $password, EntitiesBuilder $entitiesBuilder, EmailDelegate $emailDelegate)
     {
-        $this->beConstructedWith($repository);
+        $this->beConstructedWith($repository, $password, $entitiesBuilder, $emailDelegate);
+
         $this->repository = $repository;
+        $this->password = $password;
+        $this->entitiesBuilder = $entitiesBuilder;
+        $this->emailDelegate = $emailDelegate;
     }
 
     public function it_is_initializable()
@@ -40,7 +56,6 @@ class ManagerSpec extends ObjectBehavior
             ->setDeviceId($opts->getDeviceId())
             ->setSecret('abcdefgh01234567');
 
-
         $this->repository->get($opts)
             ->shouldBeCalled()
             ->willReturn($totpSecret);
@@ -54,7 +69,8 @@ class ManagerSpec extends ObjectBehavior
         $totpSecret = new TOTPSecret();
         $totpSecret->setUserGuid('123')
             ->setDeviceId('app')
-            ->setSecret('abcdefgh01234567');
+            ->setSecret('abcdefgh01234567')
+            ->setRecoveryHash('wx.yz');
 
         $this->repository->add($totpSecret)
             ->shouldBeCalled()
@@ -74,6 +90,40 @@ class ManagerSpec extends ObjectBehavior
             ->willReturn(true);
 
         $this->delete($opts)
+            ->shouldBe(true);
+    }
+
+    public function it_should_reset_totp_device_with_code()
+    {
+        $userGuid = '123';
+        $user = new User();
+        $user->guid = $userGuid;
+
+        $this->entitiesBuilder->getByUserByIndex('myusername')
+            ->willReturn($user);
+
+        $this->password->check($user, 'testpassword')
+            ->willReturn(true);
+
+        $opts = new TOTPSecretQueryOpts();
+        $opts->setUserGuid($userGuid);
+
+        $totpSecret = new TOTPSecret();
+        $totpSecret->setUserGuid('123')
+            ->setDeviceId('app')
+            ->setSecret('abcdefgh01234567')
+            ->setRecoveryHash('$2y$10$oGCiGfwd4VHmpDkiiGdBje8b0.tB6W9Cz/QCCQv1Xjp4XEtsko8um');
+
+        $this->repository->get($opts)
+            ->willReturn($totpSecret);
+
+        $this->repository->delete($opts)
+            ->willReturn(true);
+
+        $this->emailDelegate->onRecover($user)
+            ->shouldBeCalled();
+
+        $this->recover('myusername', 'testpassword', '67899876')
             ->shouldBe(true);
     }
 }
