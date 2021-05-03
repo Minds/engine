0792686cb96b251c60430bd5f5199eceaa783f46
Olivia Madrid <olivia@minds.com>
2021-05-03 09:59:16 +0000

    Pwn password changes

--- Core/Security/Module.php
+++ Core/Security/Module.php
@@ -14,6 +14,7 @@ class Module implements ModuleInterface
         Block\Module::class,
         TOTP\Module::class,
         TwoFactor\Module::class,
+        Password\Module::class,
     ];
 
     /**
