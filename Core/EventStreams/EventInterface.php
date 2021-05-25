1d30fe42372f4c35049209560f4d1412ef1a61e1
Mark Harding <mark@kramnorth.com>
2021-05-25 10:37:52 +0100

    (fix): set timestamp of events and also unawknowledge failed messages for faster redelivery

--- Core/EventStreams/EventInterface.php
+++ Core/EventStreams/EventInterface.php
@@ -3,4 +3,14 @@ namespace Minds\Core\EventStreams;
 
 interface EventInterface
 {
+    /**
+     * @param int $timestamp
+     * @return self
+     */
+    public function setTimestamp(int $timestamp): self;
+
+    /**
+     * @return int
+     */
+    public function getTimestamp(): int;
 }
