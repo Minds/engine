43e9e8cab84660011728efc1ce108f770400bf5f
Ben Hayward <ben@minds.com>
2021-07-14 08:47:30 +0000

    Allow the revoking on chat sessions #2016

--- Core/Sessions/CommonSessions/Manager.php
+++ Core/Sessions/CommonSessions/Manager.php
@@ -123,7 +123,7 @@ class Manager
                 ->setId($commonSession->getId());
 
             $response = $this->sessionsManager->delete($session);
-        } elseif ($platform === self::PLATFORM_APP) {
+        } elseif ($platform === self::PLATFORM_APP || $platform === self::PLATFORM_CHAT) {
             $token = new AccessTokenEntity();
             $token->setIdentifier($commonSession->getId());
             $token->setUserIdentifier($commonSession->getUserGuid());
