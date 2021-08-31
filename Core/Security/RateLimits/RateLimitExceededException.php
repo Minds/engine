19a52fa73595ff0def5b937576b0e497bf9d0445
Mark Harding <mark@kramnorth.com>
2021-08-31 11:43:20 +0100

    (chore): use correct error code for rate limits

--- Core/Security/RateLimits/RateLimitExceededException.php
+++ Core/Security/RateLimits/RateLimitExceededException.php
@@ -5,6 +5,9 @@ use Minds\Exceptions\UserErrorException;
 
 class RateLimitExceededException extends UserErrorException
 {
+    /** @var int */
+    protected $code = 429;
+
     /** @var string */
     protected $message = "You have exceed the rate limit. Please try again later.";
 }
