454ec811d0a58ab0d2c1ba8da47b826b76378370
Mark Harding <mark@kramnorth.com>
2021-10-04 14:28:51 +0100

    (feat): refactor iframely proxy and add support with twitter sync

---
+++ Core/Feeds/Activity/RichEmbed/Module.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Module
+ * @author mark
+ */
+
+namespace Minds\Core\Feeds\Activity\RichEmbed;
+
+use Minds\Interfaces\ModuleInterface;
+
+class Module implements ModuleInterface
+{
+    /**
+     * Executed onInit
+     * @return void
+     */
+    public function onInit(): void
+    {
+        (new Provider())->register();
+    }
+}
