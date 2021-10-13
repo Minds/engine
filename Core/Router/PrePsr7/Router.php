52626108135d6af174517c40f85326520a1965d0
Fausto Arcidiacono <fausto.arcidiacono@gmail.com>
2021-10-13 17:09:10 +0100

    (fix): removed xsrf validateRequest from pre-psr7 Router

--- Core/Router/PrePsr7/Router.php
+++ Core/Router/PrePsr7/Router.php
@@ -112,12 +112,6 @@ class Router
 
         // XSRF Cookie - may be able to remove now with OAuth flow
         $xsrf = new Security\XSRF($request, $session);
-        if ($request->getMethod() != "GET") {
-            if (!$xsrf->validateRequest()) {
-                return false;
-            }
-        }
-
         $xsrf->setCookie();
 
         if (Session::isLoggedin()) {
