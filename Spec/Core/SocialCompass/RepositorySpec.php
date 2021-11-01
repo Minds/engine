688f69702c7731d57845769754f74a02efcb756a
Fausto Arcidiacono <fausto.arcidiacono@gmail.com>
2021-11-01 12:01:37 +0000

    (feat): Run code linter

--- Spec/Core/SocialCompass/RepositorySpec.php
+++ Spec/Core/SocialCompass/RepositorySpec.php
@@ -12,15 +12,14 @@ use Spec\Minds\Mocks\Cassandra\Rows;
 
 class RepositorySpec extends ObjectBehavior
 {
-    function it_is_initializable()
+    public function it_is_initializable()
     {
         $this->shouldHaveType(Repository::class);
     }
 
     public function it_should_get_answers_and_return_an_array_with_the_entries_found(
         Client $cql
-    )
-    {
+    ) {
         $cql
             ->request(Argument::any())
             ->shouldBeCalled()
@@ -41,8 +40,7 @@ class RepositorySpec extends ObjectBehavior
 
     public function it_should_get_answers_and_return_null_when_no_entries_found(
         Client $cql
-    )
-    {
+    ) {
         $cql
             ->request(Argument::any())
             ->shouldBeCalled()
@@ -57,8 +55,7 @@ class RepositorySpec extends ObjectBehavior
 
     public function it_should_get_answers_and_return_false_when_an_error_occurred(
         Client $cql
-    )
-    {
+    ) {
         $cql
             ->request(Argument::any())
             ->shouldBeCalled()
@@ -73,8 +70,7 @@ class RepositorySpec extends ObjectBehavior
 
     public function it_should_get_answer_by_question_id_and_return_an_answer_entity_when_answer_is_found(
         Client $cql
-    )
-    {
+    ) {
         $cql
             ->request(Argument::any())
             ->shouldBeCalled()
@@ -94,8 +90,7 @@ class RepositorySpec extends ObjectBehavior
 
     public function it_should_get_answer_by_question_id_and_return_null_when_no_answer_is_found(
         Client $cql
-    )
-    {
+    ) {
         $cql
             ->request(Argument::any())
             ->shouldBeCalled()
@@ -109,8 +104,7 @@ class RepositorySpec extends ObjectBehavior
 
     public function it_should_get_answer_by_question_id_and_return_false_when_an_error_occurred(
         Client $cql
-    )
-    {
+    ) {
         $cql
             ->request(Argument::any())
             ->shouldBeCalled()
@@ -124,8 +118,7 @@ class RepositorySpec extends ObjectBehavior
 
     public function it_should_store_answers_and_return_true_if_successful(
         Client $cql
-    )
-    {
+    ) {
         $cql
             ->request(Argument::any())
             ->shouldBeCalled()
@@ -146,8 +139,7 @@ class RepositorySpec extends ObjectBehavior
 
     public function it_should_store_answers_and_return_false_if_one_or_more_answers_not_stored(
         Client $cql
-    )
-    {
+    ) {
         $cql
             ->request(Argument::any())
             ->shouldBeCalled()
@@ -164,8 +156,7 @@ class RepositorySpec extends ObjectBehavior
 
     public function it_should_update_answers_and_return_true_if_successful(
         Client $cql
-    )
-    {
+    ) {
         $cql
             ->request(Argument::any())
             ->shouldBeCalled()
@@ -186,8 +177,7 @@ class RepositorySpec extends ObjectBehavior
 
     public function it_should_update_answers_and_return_false_if_one_or_more_answers_not_stored(
         Client $cql
-    )
-    {
+    ) {
         $cql
             ->request(Argument::any())
             ->shouldBeCalled()
