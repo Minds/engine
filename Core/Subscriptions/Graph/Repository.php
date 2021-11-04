Minds/engine

commit 43a9c952e0a5da07c4d9f49152f8606e59ce766e

Filter out invalid users

Core/Subscriptions/Graph/Repository.php
@@ -91,6 +91,10 @@ class Repository
         return $response->map(function ($document) {
             $feedSyncEntity = new FeedSyncEntity();
 
+            if (!$document['_source']['username']) {
+                return null;
+            }
+
             $feedSyncEntity
                 ->setGuid($document['_source']['guid'])
                 ->setOwnerGuid($document['_source']['guid'])

Controllers/api/v1/subscribe.php
@@ -59,11 +59,11 @@ class subscribe implements Interfaces\Api
         }
         $pagingToken = (string) $users->getPagingToken();
         
-        $users = array_filter(Factory::exportable($users->toArray()), function ($user) {
-            return ($user->enabled != 'no' && $user->banned != 'yes');
+        $users = array_filter($users->toArray(), function ($user) {
+            return ($user->enabled != 'no' && $user->banned != 'yes' && $user->getUsername());
         });
 
-        $response['users'] = $users;
+        $response['users'] = Factory::exportable($users);
         $response['load-next'] = $pagingToken;
 
         return Factory::response($response);

Entities/Factory.php
@@ -74,6 +74,11 @@ class Factory
             $entity = Core\Di\Di::_()->get('Entities')->build((object) $row);
         }
 
+        // filter out invalid users
+        if ($entity instanceof User && !$entity->getUsername()) {
+            return false;
+        };
+
         if ($options['cache'] && $canBeCached && $entity && $cacheKey) {
             self::$entitiesCache[$cacheKey] = $entity;
         }

commit 6bba4e97e74eef78c5fc15011cb39e35526591cd

(fix): regression caused by 43a9c952e0a5da07c4d9f49152f8606e59ce766e - can not assume username is correctly indexed on elasticsearch. closes front#5111

Core/Subscriptions/Graph/Repository.php
@@ -91,10 +91,6 @@ class Repository
         return $response->map(function ($document) {
             $feedSyncEntity = new FeedSyncEntity();
 
-            if (!$document['_source']['username']) {
-                return null;
-            }
-
             $feedSyncEntity
                 ->setGuid($document['_source']['guid'])
                 ->setOwnerGuid($document['_source']['guid'])