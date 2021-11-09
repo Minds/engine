e940428703d79db97ce2b858f057ad429c3dd5ca
Ben Hayward <ben@minds.com>
2021-11-09 16:13:07 +0000

    Accidental pushes to settings example

--- settings.example.php
+++ settings.example.php
@@ -264,8 +264,6 @@ $CONFIG->set('facebook', [
 $CONFIG->set('twitter', [
     'api_key' => '{{twitter-app-id}}',
     'api_secret' => '{{twitter-app-id}}',
-    'bearer_token' => '',
-    'min_followers_for_sync' => 25000,
 ]);
 
 $CONFIG->set('twilio', [
@@ -553,10 +551,6 @@ $CONFIG->set('features', [
 ]);
 
 $CONFIG->set('email', [
-    'sender' => [
-        'email' => 'no-reply@minds.com',
-        'name' => 'Minds'
-    ],
     'smtp' => [
         'host' => '',
         'username' => '',
