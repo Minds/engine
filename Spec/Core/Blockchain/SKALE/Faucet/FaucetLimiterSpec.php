b85dc61fa0566cbb792a2e5243bed0b8b13d04eb
Ben Hayward <ben@minds.com>
2021-12-13 16:43:38 +0000

    Adding in spec tests

---
+++ Spec/Core/Blockchain/SKALE/Faucet/FaucetLimiterSpec.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Spec\Minds\Core\Blockchain\SKALE\Faucet;
+
+use Minds\Core\Blockchain\SKALE\Faucet\FaucetLimiter;
+use PhpSpec\ObjectBehavior;
+use Minds\Core\Security\RateLimits\KeyValueLimiter;
+use Minds\Core\Security\RateLimits\RateLimitExceededException;
+use Minds\Entities\User;
+use Prophecy\Argument;
+
+class FaucetLimiterSpec extends ObjectBehavior
+{
+    /** @var KeyValueLimiter */
+    protected $kvLimiter;
+
+    public function let(
+        KeyValueLimiter $kvLimiter,
+    ) {
+        $this->beConstructedWith($kvLimiter);
+        $this->kvLimiter = $kvLimiter;
+    }
+
+    public function it_is_initializable()
+    {
+        $this->shouldHaveType(FaucetLimiter::class);
+    }
+
+    public function it_should_return_true_if_no_limits_exceeded(
+        User $user
+    ) {
+        $user->getPhoneNumberHash()->willReturn('123');
+        $user->getGuid()->willReturn('321');
+
+        $this->kvLimiter->setKey('skale:faucet:321')->shouldBeCalledTimes(1)->willReturn($this->kvLimiter);
+        $this->kvLimiter->setKey('skale:faucet:0x123')->shouldBeCalledTimes(1)->willReturn($this->kvLimiter);
+        $this->kvLimiter->setKey('skale:faucet:123')->shouldBeCalledTimes(1)->willReturn($this->kvLimiter);
+
+        $this->kvLimiter->setValue(Argument::any())->willReturn($this->kvLimiter);
+        $this->kvLimiter->setSeconds(Argument::any())->willReturn($this->kvLimiter);
+        $this->kvLimiter->setMax(Argument::any())->willReturn($this->kvLimiter);
+        $this->kvLimiter->checkAndIncrement()->willReturn(true);
+
+        $this->checkAndIncrement($user, '0x123')->shouldReturn(true);
+    }
+
+    public function it_should_throw_error_out_if_a_limit_is_exceeded(
+        User $user,
+    )
+    {
+        $user->getPhoneNumberHash()->willReturn('123');
+        $user->getEthWallet()->willReturn('0x123');
+        $user->getGuid()->willReturn('321');
+
+        $this->kvLimiter->setKey('skale:faucet:321')->shouldBeCalledTimes(1)->willReturn($this->kvLimiter);
+
+        $this->kvLimiter->setValue(Argument::any())->willReturn($this->kvLimiter);
+        $this->kvLimiter->setSeconds(Argument::any())->willReturn($this->kvLimiter);
+        $this->kvLimiter->setMax(Argument::any())->willReturn($this->kvLimiter);
+        $this->kvLimiter->checkAndIncrement()->willThrow(new RateLimitExceededException());
+
+        $this
+            ->shouldThrow(RateLimitExceededException::class)
+            ->during("checkAndIncrement", [$user, '0x123']);
+    }
+}
---
+++ Spec/Core/Blockchain/SKALE/ManagerSpec.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace Spec\Minds\Core\Blockchain\SKALE;
+
+use Minds\Core\Blockchain\Services\Web3Services\MindsSKALEWeb3Service;
+use Minds\Core\Blockchain\SKALE\Faucet\FaucetLimiter;
+use PhpSpec\ObjectBehavior;
+use Minds\Core\Blockchain\SKALE\Manager;
+use Minds\Core\Config;
+use Minds\Entities\User;
+use Minds\Exceptions\ServerErrorException;
+use Minds\Exceptions\UserErrorException;
+
+class ManagerSpec extends ObjectBehavior
+{
+    /** @var MindsSKALEWeb3Service */
+    protected $skaleWeb3Service;
+
+    /** @var FaucetLimiter */
+    protected $faucetLimiter;
+
+    /** @var Config */
+    protected $config;
+
+    public function let(
+        MindsSKALEWeb3Service $skaleWeb3Service,
+        FaucetLimiter $faucetLimiter,
+        Config $config
+    ) {
+        $this->beConstructedWith(
+            $skaleWeb3Service,
+            $faucetLimiter,
+            $config
+        );
+        $this->skaleWeb3Service = $skaleWeb3Service;
+        $this->faucetLimiter = $faucetLimiter;
+        $this->config = $config;
+    }
+
+    public function it_is_initializable()
+    {
+        $this->shouldHaveType(Manager::class);
+    }
+
+    public function it_should_pass_request_to_faucet_if_rate_limit_not_imposed(
+        User $user,
+    ) {
+        $requestAddress = '0x123';
+        $responseAddress = '0x456';
+
+        $this->faucetLimiter->checkAndIncrement($user, $requestAddress)
+            ->shouldBeCalled()
+            ->willReturn(true);    
+        
+        $this->skaleWeb3Service->requestFromSKETHFaucet($requestAddress)
+            ->shouldBeCalled()
+            ->willReturn($responseAddress);
+
+        $this->requestFromFaucet($user, $requestAddress)->shouldReturn($responseAddress);
+    }
+
+    public function it_should_get_wallet_from_user_if_no_wallet_passed(
+        User $user,
+    ) {
+        $requestAddress = '0x123';
+        $responseAddress = '0x456';
+
+        $user->getEthWallet()
+            ->shouldBeCalled()
+            ->willReturn($requestAddress);
+
+        $this->faucetLimiter->checkAndIncrement($user, $requestAddress)
+            ->shouldBeCalled()
+            ->willReturn(true);    
+        
+        $this->skaleWeb3Service->requestFromSKETHFaucet($requestAddress)
+            ->shouldBeCalled()
+            ->willReturn($responseAddress);
+
+        $this->requestFromFaucet($user, null)->shouldReturn($responseAddress);
+    }
+
+    public function it_should_not_pass_request_to_faucet_if_rate_limit_set(
+        User $user,
+    ) {
+        $requestAddress = '0x123';
+        $this->faucetLimiter->checkAndIncrement($user, $requestAddress)
+            ->shouldBeCalled()
+            ->willThrow(new ServerErrorException()); 
+    
+        $this->skaleWeb3Service->requestFromSKETHFaucet($requestAddress)
+            ->shouldNotBeCalled();
+
+        $this
+            ->shouldThrow(ServerErrorException::class)
+            ->during("requestFromFaucet", [$user, $requestAddress]);
+    }
+
+    public function it_should_not_pass_request_to_faucet_if_no_address(
+        User $user,
+    ) {
+        $requestAddress = '0x123';
+        $responseAddress = '0x456';
+
+        $user->getEthWallet()
+            ->shouldBeCalled()
+            ->willReturn(null);
+
+        $this->faucetLimiter->checkAndIncrement($user, $requestAddress)
+            ->shouldNotBeCalled()
+            ->willReturn(true);
+
+        $this
+            ->shouldThrow(UserErrorException::class)
+            ->during("requestFromFaucet", [$user, null]);
+    }
+}
