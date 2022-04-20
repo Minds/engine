32a7469f51d7606c080ca1f4de593b9206186815
Ben Hayward <ben@minds.com>
2022-04-20 12:59:11 +0100

    Updating CLI controller to help debugging and testing on prod

--- Controllers/Cli/BigQuery.php
+++ Controllers/Cli/BigQuery.php
@@ -5,6 +5,7 @@ namespace Minds\Controllers\Cli;
 use Minds\Cli;
 use Minds\Core\Blockchain\BigQuery\HoldersQuery;
 use Minds\Interfaces;
+use Minds\Core\Blockchain\Wallets\OnChain\UniqueOnChain\Manager as UniqueOnChainManager;
 
 /**
  * CLI for manually running BigQuery queries.
@@ -32,23 +33,51 @@ class BigQuery extends Cli\Controller implements Interfaces\CliControllerInterfa
 
     /**
      * Gets information on rows and time taken without dumping query results.
+     *
+     * # Usage Example
+     * ## Count time and rows for ALL addresses found
+     * php cli.php BigQuery getHolders
+     *
+     * ## Count time and rows for all addresses linked to a registered account.
+     * php cli.php BigQuery getHolders --onlyRegistered=true
+     *
+     * ## Count time and rows for all addresses linked to a registered account.
+     * ## Use verbose flag which will dump the holders object.
+     * php cli.php BigQuery getHolders --onlyRegistered=true --verbose=true
+     *
      * @return void
      */
-    public function getHoldersWithBalances(): void
+    public function getHolders(): void
     {
-        error_log("Getting all holders with balances...");
+        $onlyRegistered = $this->getOpt('onlyRegistered');
+        $verbose = $this->getOpt('verbose');
+
+        $uniqueOnChainManager = new UniqueOnChainManager();
+
+        if ($onlyRegistered) {
+            $this->out("Getting all holders with balances who are on-site...");
+        } else {
+            $this->out("Getting all holders with balances...");
+        }
+
         $start = hrtime(true);
 
-        $manager = new HoldersQuery();
-        $result = $manager->get();
+        $directHoldersQuery = new HoldersQuery();
+
+        $result = $onlyRegistered ?
+            $uniqueOnChainManager->getAll() :
+            $directHoldersQuery->get();
 
         $count = 0;
         foreach ($result as $holder) {
+            if ($verbose) {
+                $this->out("Row $count: " . var_export($holder, true));
+            }
             $count++;
         }
 
         $timeTaken = (hrtime(true) - $start) / 1e+6;
-        error_log("The operation took $timeTaken ms...");
-        error_log("Iterated over $count rows.");
+        $this->out("The operation took $timeTaken ms...");
+        $this->out("Iterated over $count rows.");
     }
 }
