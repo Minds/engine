63077699a745093f86c9750a9d5f25483fd85015
Fausto Arcidiacono <fausto.arcidiacono@gmail.com>
2022-04-28 16:11:43 +0000

    (fix): Added exception logging exclusions for Sentry minds/front#5445

--- Exceptions/StringLengthException.php
+++ Exceptions/StringLengthException.php
@@ -1,9 +1,11 @@
 <?php
 namespace Minds\Exceptions;
 
+use Minds\Interfaces\SentryExceptionExclusionInterface;
+
 /**
  * Exception thrown when string length is determined to be invalid.
  */
-class StringLengthException extends UserErrorException
+class StringLengthException extends UserErrorException implements SentryExceptionExclusionInterface
 {
 }
