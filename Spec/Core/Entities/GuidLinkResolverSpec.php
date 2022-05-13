16961ee6c76d8a4ed3bf7fc11167595505203071
Ben Hayward <ben@minds.com>
2022-05-13 12:57:21 +0100

    Fix broken test

--- Spec/Core/Entities/GuidLinkResolverSpec.php
+++ Spec/Core/Entities/GuidLinkResolverSpec.php
@@ -31,8 +31,8 @@ class GuidLinkResolverSpec extends ObjectBehavior
 
     public function it_should_resolve_linked_activity_from_entity_guid()
     {
-        $entityGuid = 123;
-        $activityGuid = 321;
+        $entityGuid = "123";
+        $activityGuid = "321";
 
         $this->db->getRow('activity:entitylink:123')
             ->shouldBeCalled()
@@ -47,8 +47,8 @@ class GuidLinkResolverSpec extends ObjectBehavior
     public function it_should_resolve_linked_entity_from_activity_guid(
         Activity $activity
     ) {
-        $activityGuid = 321;
-        $entityGuid = 123;
+        $activityGuid = "321";
+        $entityGuid = "123";
 
         // mocking no linked activity found because this IS an activity guid
         $this->db->getRow('activity:entitylink:321')
@@ -69,7 +69,7 @@ class GuidLinkResolverSpec extends ObjectBehavior
     public function it_should_return_null_if_no_match_is_found(
         Activity $activity
     ) {
-        $activityGuid = 321;
+        $activityGuid = "321";
 
         // mocking no linked activity found because this IS an activity guid
         $this->db->getRow('activity:entitylink:321')
