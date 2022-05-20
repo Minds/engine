e9e82a715477cc05d39f1a37c6032030bea6d5c4
Mani Shooshtari <mani@shooshtari.com>
2022-05-20 13:33:34 +0300

    revert block entry changes

--- Core/Security/Block/BlockEntry.php
+++ Core/Security/Block/BlockEntry.php
@@ -21,9 +21,7 @@ class BlockEntry
      */
     public function setActor($actor): self
     {
-        if (is_string($actor)) {
-            $this->actorGuid = $actor;
-        } elseif ($actor instanceof User) {
+        if ($actor instanceof User) {
             $this->actorGuid = (string) $actor->getGuid();
         } else {
             // If this isn't a user, then assume its a standard entity
@@ -40,9 +38,7 @@ class BlockEntry
      */
     public function setSubject($subject): self
     {
-        if (is_string($subject)) {
-            $this->subjectGuid = $subject;
-        } elseif ($subject instanceof User) {
+        if ($subject instanceof User) {
             $this->subjectGuid = (string) $subject->getGuid();
         } else {
             // If this isn't a user, then assume its a standard entity
