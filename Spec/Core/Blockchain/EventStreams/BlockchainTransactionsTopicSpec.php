df41382f2a14a4ce58609dc46d70d2c2f082bafc
Ben Hayward <ben@minds.com>
2022-07-27 15:44:58 +0100

    Add send tests for transaction topic

---
+++ Spec/Core/Blockchain/EventStreams/BlockchainTransactionsTopicSpec.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Spec\Minds\Core\Blockchain\EventStreams;
+
+use Minds\Core\Blockchain\EventStreams\BlockchainTransactionEvent;
+use Minds\Core\Blockchain\EventStreams\BlockchainTransactionsTopic;
+use PhpSpec\ObjectBehavior;
+
+class BlockchainTransactionsTopicSpec extends ObjectBehavior
+{
+    public function it_is_initializable()
+    {
+        $this->shouldHaveType(BlockchainTransactionsTopic::class);
+    }
+
+    public function it_should_send_event()
+    {
+        $senderGuid = '012345';
+        $receiverGuid = '98765';
+        $transactionId = '0x1111111';
+        $amountWei ='100000000000';
+        $walletAddress = '0x2222222';
+        $contract = 'offchain:wire';
+
+        $event = new BlockchainTransactionEvent();
+        $event->setSenderGuid($senderGuid)
+            ->setReceiverGuid($receiverGuid)
+            ->setTransactionId($transactionId)
+            ->setWalletAddress($walletAddress)
+            ->setContract($contract)
+            ->setAmountWei($amountWei);
+
+        $this->send($event)
+            ->shouldBe(true);
+    }
+}
