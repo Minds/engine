c757c9d39dfb27495f5e3a99c2af9abdaf78a3d9
Ben Hayward <ben@minds.com>
2022-07-27 14:47:53 +0100

    Missing docs

--- Core/Blockchain/Transactions/Delegates/TransactionEventDelegate.php
+++ Core/Blockchain/Transactions/Delegates/TransactionEventDelegate.php
@@ -6,14 +6,26 @@ use Minds\Core\Blockchain\EventStreams\BlockchainTransactionEvent;
 use Minds\Core\Blockchain\EventStreams\BlockchainTransactionsTopic;
 use Minds\Core\Blockchain\Transactions\Transaction;
 
+/**
+ * Delegate for firing transaction events.
+ */
 class TransactionEventDelegate
 {
+    /**
+     * Constructor.
+     * @param BlockchainTransactionsTopic|null $blockchainTransactionsTopic - topic to fire events to.
+     */
     public function __construct(
         private ?BlockchainTransactionsTopic $blockchainTransactionsTopic = null
     ) {
         $this->blockchainTransactionsTopic ??= new BlockchainTransactionsTopic();
     }
 
+    /**
+     * Called on transaction add. Sends event to topic.
+     * @param Transaction $transaction - added transaction.
+     * @return void
+     */
     public function onAdd(Transaction $transaction): void
     {
         $this->blockchainTransactionsTopic->send(
@@ -21,6 +33,11 @@ class TransactionEventDelegate
         );
     }
 
+    /**
+     * Construct an transaction event.
+     * @param Transaction $transaction - transaction to construct event from.
+     * @return BlockchainTransactionEvent - built event object ready to be fired to topic.
+     */
     private function buildEvent(Transaction $transaction): BlockchainTransactionEvent
     {
         return (new BlockchainTransactionEvent())
