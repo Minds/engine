f42e2e668626f46103578330db751bd85fa4c099
Mark Harding <mark@minds.com>
2022-08-09 16:31:51 +0000

    Quotes count cache - front#5673

--- Core/Feeds/Activity/Delegates/MetricsDelegate.php
+++ Core/Feeds/Activity/Delegates/MetricsDelegate.php
@@ -53,10 +53,14 @@ class MetricsDelegate
 
             $event->push();
 
-            // Purge counter cache
-            $this->interactionCounters
-                ->setCounter(InteractionCounters::COUNTER_QUOTES)
-                ->purgeCache($activity);
+            if ($activity->isQuotedPost()) {
+                // Purge counter cache
+                $quotesCounter = $this->interactionCounters->setCounter(InteractionCounters::COUNTER_QUOTES);
+                $currentCount = (int) $quotesCounter->get($remind, readFromCache: false, saveToCache: false); // $redmind = quote post too.
+                // hacky solution, increment the cache. purging the cache will show incorrect results
+                // as it needs to wait for refresh_interval to clear on elasticsearch, post indexing
+                $quotesCounter->updateCache($remind, $currentCount + 1);
+            }
         }
 
         if ($activity->isRemind() && isset($remind)) {
