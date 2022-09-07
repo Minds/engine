b0cdc4f8705fe5d0119a2b2497c45bd210895756
Fausto Arcidiacono <fausto@minds.com>
2022-09-07 10:05:05 +0100

    (feat): Changes to integrations tests pipeline reqs and modified Supermind req to accept username instead of guid

--- Core/Feeds/Activity/Manager.php
+++ Core/Feeds/Activity/Manager.php
@@ -7,6 +7,7 @@
 
 namespace Minds\Core\Feeds\Activity;
 
+use Exception;
 use Minds\Common\EntityMutation;
 use Minds\Common\Urn;
 use Minds\Core\Boost\Network\ElasticRepository as BoostElasticRepository;
@@ -31,6 +32,9 @@ use Minds\Core\Supermind\Validators\SupermindRequestValidator;
 use Minds\Entities\Activity;
 use Minds\Entities\Entity;
 use Minds\Entities\EntityInterface;
+use Minds\Entities\User;
+use Minds\Entities\ValidationError;
+use Minds\Entities\ValidationErrorCollection;
 use Minds\Exceptions\StopEventException;
 use Minds\Exceptions\UserErrorException;
 use Minds\Helpers\StringLengthValidators\MessageLengthValidator;
@@ -183,13 +187,26 @@ class Manager
                 errors: $validator->getErrors()
             );
         }
+        try {
+            $receiverUser = new User($supermindDetails['supermind_request']['receiver_guid']);
+        } catch (Exception $e) {
+            throw new UserErrorException(
+                message: "An error was encountered whilst validating the request",
+                code: 400,
+                errors: (new ValidationErrorCollection())->add(
+                    new ValidationError(
+                        "supermind_request:receiver_guid"
+                    )
+                )
+            );
+        }
 
         $paymentMethodId = $supermindDetails['supermind_request']['payment_options']['payment_method_id'] ?? null;
 
         $supermindRequest = (new SupermindRequest())
             ->setGuid(Guid::build())
             ->setSenderGuid((string)$activity->owner_guid)
-            ->setReceiverGuid((string) $supermindDetails['supermind_request']['receiver_guid'])
+            ->setReceiverGuid((string) $receiverUser->getGuid())
             ->setReplyType($supermindDetails['supermind_request']['reply_type'])
             ->setTwitterRequired($supermindDetails['supermind_request']['twitter_required'])
             ->setPaymentAmount($supermindDetails['supermind_request']['payment_options']['amount'])
