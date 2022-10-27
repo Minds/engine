a75e85aa0ece16b11332aa6853324780f62a4288
Mark Harding <mark@minds.com>
2022-10-27 15:24:52 +0100

    (fix): linting

--- Spec/Core/Payments/Stripe/Checkout/ControllerSpec.php
+++ Spec/Core/Payments/Stripe/Checkout/ControllerSpec.php
@@ -7,7 +7,7 @@ use PhpSpec\ObjectBehavior;
 
 class ControllerSpec extends ObjectBehavior
 {
-    function it_is_initializable()
+    public function it_is_initializable()
     {
         $this->shouldHaveType(Controller::class);
     }
