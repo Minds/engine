3fc533aee4e0df610b038cb1f20cda905f9cd489
Fausto Arcidiacono <fausto@minds.com>
2022-11-03 10:54:41 +0000

    (chore): Added acl check to single supermind request endpoint

--- Core/Supermind/Manager.php
+++ Core/Supermind/Manager.php
@@ -507,11 +507,17 @@ class Manager
     /**
      * @param string $supermindRequestId
      * @return SupermindRequest
+     * @throws ForbiddenException
      * @throws SupermindNotFoundException
      */
     public function getRequest(string $supermindRequestId): SupermindRequest
     {
         $supermindRequest = $this->repository->getSupermindRequest($supermindRequestId) ?? throw new SupermindNotFoundException();
+
+        if (!$this->acl->read($supermindRequest, $this->user)) {
+            throw new ForbiddenException();
+        }
+
         $supermindRequest->setEntity($this->entitiesBuilder->single($supermindRequest->getActivityGuid()));
         $supermindRequest->setReceiverEntity($this->entitiesBuilder->single($supermindRequest->getReceiverGuid()));
 
