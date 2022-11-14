ee4fd0aea9adf8536b478337919ad306623b61e8
Fausto Arcidiacono <fausto.arcidiacono@gmail.com>
2022-11-14 09:36:33 +0000

    Feat/supermind twitter reply minds#3476

--- Spec/Core/Supermind/ManagerSpec.php
+++ Spec/Core/Supermind/ManagerSpec.php
@@ -1518,6 +1518,10 @@ class ManagerSpec extends ObjectBehavior
             ->shouldBeCalled()
             ->willReturn($activityGuid);
 
+        $supermindRequest->getTwitterRequired()
+            ->shouldBeCalled()
+            ->willReturn(false);
+
         $supermindRequest->getReceiverGuid()
             ->shouldBeCalled()
             ->willReturn($receiverGuid);
