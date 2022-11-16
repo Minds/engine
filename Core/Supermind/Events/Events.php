18a2ca14137401c2a7ad2d59e56e9ce36f46e039
Olivia Madrid <oliviajmadrid@gmail.com>
2022-11-15 17:34:34 -0700

    supermind receiever and reply guid in activity export

--- Core/Supermind/Events/Events.php
+++ Core/Supermind/Events/Events.php
@@ -7,13 +7,17 @@ namespace Minds\Core\Supermind\Events;
 use Minds\Core\Di\Di;
 use Minds\Core\Events\Event;
 use Minds\Core\Events\EventsDispatcher;
+use Minds\Core\Supermind\Manager;
 use Minds\Core\Supermind\Models\SupermindRequest;
 use Minds\Entities\User;
+use Minds\Core\EntitiesBuilder;
 
 class Events
 {
     public function __construct(
-        private ?EventsDispatcher $eventsDispatcher = null
+        private ?EventsDispatcher $eventsDispatcher = null,
+        private ?Manager $supermindManager = null,
+        private ?EntitiesBuilder $entitiesBuilder = null
     ) {
         $this->eventsDispatcher ??= Di::_()->get('EventsDispatcher');
     }
@@ -21,6 +25,7 @@ class Events
     public function register(): void
     {
         $this->supermindACLEvents();
+        $this->supermindExportEvents();
     }
 
     private function supermindACLEvents(): void
@@ -81,6 +86,52 @@ class Events
             ) {
                 $event->setResponse(true);
             }
+        });
+    }
+
+    private function supermindExportEvents(): void
+    {
+        $this->eventsDispatcher->register('export:extender', 'activity', function ($event) {
+            if (!$this->supermindManager) {
+                $this->supermindManager = Di::_()->get('Supermind\Manager');
+            }
+
+            $params = $event->getParameters();
+            $activity = $params['entity'];
+            $export = $event->response() ?: [];
+
+            $supermindRequest = $activity->getSupermind();
+            if (!$supermindRequest) {
+                return;
+            }
+
+            $isReply = $supermindRequest['is_reply'];
+            if ($isReply) {
+                return;
+            }
+
+            $requestGuid = $supermindRequest['request_guid'];
+
+            $request = $this->supermindManager->getRequest($requestGuid);
+
+            $receiverGuid = $request->getReceiverGuid();
+
+            if (!$this->entitiesBuilder) {
+                $this->entitiesBuilder = Di::_()->get('EntitiesBuilder');
+            }
+
+            $receiverUser = $this->entitiesBuilder->single($receiverGuid);
+            if (!$receiverUser instanceof User) {
+                return;
+            }
+
+            $replyGuid = $request->getReplyActivityGuid();
+
+            $export['supermind'] = $supermindRequest;
+            $export['supermind']['receiver_user'] = $receiverUser->export();
+            $export['supermind']['reply_guid'] = $replyGuid;
+
+            $event->setResponse($export);
         });
     }
 }
