1372368862f00d18b432c5d516abb6f0c52a43f7
Mark Harding <mark@minds.com>
2022-11-21 13:27:13 +0000

    (fix): failing feeds

--- Core/Supermind/Events/Events.php
+++ Core/Supermind/Events/Events.php
@@ -7,17 +7,19 @@ namespace Minds\Core\Supermind\Events;
 use Minds\Core\Di\Di;
 use Minds\Core\Events\Event;
 use Minds\Core\Events\EventsDispatcher;
-use Minds\Core\Supermind\Manager;
+use Minds\Core\Supermind\Manager as SupermindManager;
 use Minds\Core\Supermind\Models\SupermindRequest;
 use Minds\Entities\User;
 use Minds\Core\EntitiesBuilder;
+use Minds\Core\Security\ACL;
 
 class Events
 {
     public function __construct(
         private ?EventsDispatcher $eventsDispatcher = null,
-        private ?Manager $supermindManager = null,
-        private ?EntitiesBuilder $entitiesBuilder = null
+        private ?SupermindManager $supermindManager = null,
+        private ?EntitiesBuilder $entitiesBuilder = null,
+        private ?ACL $acl = null,
     ) {
         $this->eventsDispatcher ??= Di::_()->get('EventsDispatcher');
     }
@@ -92,19 +94,19 @@ class Events
     private function supermindExportEvents(): void
     {
         $this->eventsDispatcher->register('export:extender', 'activity', function ($event) {
-            if (!$this->supermindManager) {
-                $this->supermindManager = Di::_()->get('Supermind\Manager');
-            }
-
             $params = $event->getParameters();
             $activity = $params['entity'];
             $export = $event->response() ?: [];
 
+            // Contains the request guid and other metadata we may need
             $supermindRequest = $activity->getSupermind();
+
+            // If not a supermind, skip
             if (!$supermindRequest) {
                 return;
             }
 
+            // We dont care if this is a reply, so skip
             $isReply = $supermindRequest['is_reply'];
             if ($isReply) {
                 return;
@@ -112,26 +114,46 @@ class Events
 
             $requestGuid = $supermindRequest['request_guid'];
 
-            $request = $this->supermindManager->getRequest($requestGuid);
+            // We need to bypass acl to get info about the supermind
+            $ia = $this->getAcl()->setIgnore(true);
+
+            // Fetch the supermind
+            $request = $this->getSupermindManager()->getRequest($requestGuid);
+
+            // Set ACL back to what it was
+            $this->getAcl()->setIgnore($ia);
 
             $receiverGuid = $request->getReceiverGuid();
 
-            if (!$this->entitiesBuilder) {
-                $this->entitiesBuilder = Di::_()->get('EntitiesBuilder');
-            }
-
-            $receiverUser = $this->entitiesBuilder->single($receiverGuid);
+            // Build the receiving user
+            $receiverUser = $this->getEntitiesBuilder()->single($receiverGuid);
             if (!$receiverUser instanceof User) {
                 return;
             }
 
             $replyGuid = $request->getReplyActivityGuid();
 
+            // Construct the export output
             $export['supermind'] = $supermindRequest;
             $export['supermind']['receiver_user'] = $receiverUser->export();
             $export['supermind']['reply_guid'] = $replyGuid;
 
             $event->setResponse($export);
         });
+    }
+
+    private function getEntitiesBuilder(): EntitiesBuilder
+    {
+        return $this->entitiesBuilder ??= Di::_()->get('EntitiesBuilder');
+    }
+
+    private function getAcl(): ACL
+    {
+        return $this->acl ??= Di::_()->get('Security\ACL');
+    }
+
+    private function getSupermindManager(): SupermindManager
+    {
+        return $this->supermindManager ??= Di::_()->get('Supermind\Manager');
     }
 }
--- Spec/Core/Supermind/Events/EventsSpec.php
+++ Spec/Core/Supermind/Events/EventsSpec.php
@@ -2,21 +2,44 @@
 
 namespace Spec\Minds\Core\Supermind\Events;
 
+use Minds\Core\Di\Di;
+use Minds\Core\EntitiesBuilder;
 use Minds\Core\Events\EventsDispatcher;
+use Minds\Core\Security\ACL;
+use Minds\Core\Supermind\Manager as SupermindManager;
 use Minds\Core\Supermind\Events\Events;
+use Minds\Core\Supermind\Models\SupermindRequest;
+use Minds\Entities\Activity;
+use Minds\Entities\User;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
 class EventsSpec extends ObjectBehavior
 {
     /** @var EventsDispatcher */
-    private $eventsDispatcher;
+    private $eventsDispatcherMock;
 
-    public function let(EventsDispatcher $eventsDispatcher)
-    {
-        $this->beConstructedWith($eventsDispatcher);
+    /** @var SupermindManager */
+    private $supermindManagerMock;
 
-        $this->eventsDispatcher = $eventsDispatcher;
+    /** @var EntitiesBuilder */
+    private $entitiesBuilderMock;
+
+    /** @var ACL */
+    private $aclMock;
+
+    public function let(
+        EventsDispatcher $eventsDispatcherMock,
+        SupermindManager $supermindManagerMock,
+        EntitiesBuilder $entitiesBuilderMock,
+        ACL $aclMock
+    ) {
+        $this->beConstructedWith(null, $supermindManagerMock, $entitiesBuilderMock, $aclMock);
+
+        $this->eventsDispatcherMock = $eventsDispatcherMock;
+        $this->supermindManagerMock = $supermindManagerMock;
+        $this->entitiesBuilderMock = $entitiesBuilderMock;
+        $this->aclMock = $aclMock;
     }
 
     public function it_is_initializable()
@@ -26,15 +49,42 @@ class EventsSpec extends ObjectBehavior
 
     public function it_should_register_events()
     {
-        $this->eventsDispatcher->register('acl:read', 'supermind', Argument::type('callable'))
+        $this->beConstructedWith($this->eventsDispatcherMock);
+
+        $this->eventsDispatcherMock->register('acl:read', 'supermind', Argument::type('callable'))
             ->shouldBeCalled();
 
-        $this->eventsDispatcher->register('acl:write', 'supermind', Argument::type('callable'))
+        $this->eventsDispatcherMock->register('acl:write', 'supermind', Argument::type('callable'))
             ->shouldBeCalled();
 
-        $this->eventsDispatcher->register('export:extender', 'activity', Argument::type('callable'))
+        $this->eventsDispatcherMock->register('export:extender', 'activity', Argument::type('callable'))
             ->shouldBeCalled();
 
         $this->register();
+    }
+
+    public function it_should_extend_activity_export_for_supermind_request(SupermindRequest $supermindRequest, User $receiverUser)
+    {
+        $this->register(); // Setup hooks
+
+        $this->aclMock->setIgnore(true)->shouldBeCalled()->willReturn(false);
+        $this->aclMock->setIgnore(false)->shouldBeCalled();
+
+        $this->supermindManagerMock->getRequest('123')
+            ->willReturn($supermindRequest);
+
+        $supermindRequest->getReceiverGuid()->willReturn('456');
+        $supermindRequest->getReplyActivityGuid()->willReturn('789');
+
+        $this->entitiesBuilderMock->single('456')
+            ->willReturn($receiverUser);
+
+        $activity = new Activity();
+        $activity->setSupermind([
+            'request_guid' => '123',
+            'is_reply' => false,
+        ]);
+        
+        $response = Di::_()->get('EventsDispatcher')->trigger('export:extender', 'activity', ['entity'=>$activity], []);
     }
 }
