a413359e0792697c2fb4ff985aef17ea4235b4d9
Zack Wynne <zack.wynne@icloud.com>
2022-12-14 10:20:33 +0000

    (infra): Support Querying Delegated Nostr Events (NIP-26)

--- Core/Nostr/Manager.php
+++ Core/Nostr/Manager.php
@@ -125,6 +125,16 @@ class Manager
     }
 
     /**
+     * Will return a delegate pubkey from a delegator public key
+     * @param string $delegator_pubkey
+     * @return string|null
+     */
+    public function getNip26Delegate(string $delegator_pubkey): ?string
+    {
+        return $this->repository->getNip26Delegate($delegator_pubkey);
+    }
+
+    /**
      * Will build a signed Nostr event
      * https://github.com/nostr-protocol/nips/blob/master/01.md#events-and-signatures
      * @param EntityInterface $entity
