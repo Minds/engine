e7e64c50eb46500e7b3829c238ad402eeffc63a2
Mark Harding <mark@minds.com>
2023-01-24 12:17:12 +0000

    Support aggregating views for boosts - #2495

--- Core/Boost/V3/Summaries/Repository.php
+++ Core/Boost/V3/Summaries/Repository.php
@@ -3,6 +3,32 @@ declare(strict_types=1);
 
 namespace Minds\Core\Boost\V3\Summaries;
 
+use DateTime;
+use Minds\Core\Data\MySQL\Client;
+
 class Repository
 {
+    public function __construct(private ?Client $mysqlClient = null)
+    {
+        $this->mysqlClient ??= new Client();
+    }
+
+    /**
+     * @param string $guid
+     * @param DateTime $date
+     * @param int $views
+     * @return bool
+     */
+    public function add(string $guid, DateTime $date, int $views): bool
+    {
+        $statement = "INSERT INTO boost_summaries (guid, date, views) VALUES (:guid,:date,:views) ON DUPLICATE KEY UPDATE views=:views";
+        $values = [
+            'guid' => $guid,
+            'date' => $date->format('c'),
+            'views' => $views
+        ];
+
+        $stmt = $this->mysqlClient->getConnection(Client::CONNECTION_MASTER)->prepare($statement);
+        return $stmt->execute($values);
+    }
 }
