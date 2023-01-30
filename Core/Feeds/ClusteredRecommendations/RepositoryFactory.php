4e4bf22f848921e77afece7f8dcf88322f4872c6
Fausto Arcidiacono <fausto.arcidiacono@gmail.com>
2023-01-30 17:28:01 +0000

    (feat): Implemented new user recommendations based on feature flag minds/engine#2494

--- Core/Feeds/ClusteredRecommendations/RepositoryFactory.php
+++ Core/Feeds/ClusteredRecommendations/RepositoryFactory.php
@@ -10,6 +10,7 @@ class RepositoryFactory
     public function getInstance(string $type): RepositoryInterface
     {
         return match ($type) {
+            LegacyMySQLRepository::class => new LegacyMySQLRepository(),
             MySQLRepository::class => new MySQLRepository(),
             default => new ElasticSearchRepository()
         };
