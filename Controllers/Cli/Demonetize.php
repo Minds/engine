32cd84739766f6ea27bd52e5a83b94a10627a0f0
Ben Hayward <ben@minds.com>
2023-02-09 16:46:15 +0000

    Update CLI

--- Controllers/Cli/Demonetize.php
+++ Controllers/Cli/Demonetize.php
@@ -1,21 +1,29 @@
 <?php
+declare(strict_types=1);
 
 namespace Minds\Controllers\Cli;
 
 use Minds\Cli;
+use Minds\Core\Demonetization\DemonetizationContext;
+use Minds\Core\Demonetization\Strategies\DemonetizePostStrategy;
 use Minds\Core\Demonetization\Strategies\Interfaces\DemonetizableEntityInterface;
 use Minds\Core\Settings\Manager as UserSettingsManager;
 use Minds\Core\Di\Di;
 use Minds\Core\EntitiesBuilder;
+use Minds\Core\Security\ACL;
 use Minds\Interfaces;
 
 class Demonetize extends Cli\Controller implements Interfaces\CliControllerInterface
 {
     public function __construct(
         private ?EntitiesBuilder $entitiesBuilder = null,
+        private ?DemonetizationContext $demonetizationContext = null,
+        private ?DemonetizePostStrategy $demonetizePostStrategy = null,
         private ?UserSettingsManager $userSettingsManager = null,
     ) {
         $this->entitiesBuilder ??= Di::_()->get('EntitiesBuilder');
+        $this->demonetizationContext ??= Di::_()->get(DemonetizationContext::class);
+        $this->demonetizePostStrategy ??= Di::_()->get(DemonetizePostStrategy::class);
         $this->userSettingsManager ??= Di::_()->get('Settings\Manager');
     }
 
@@ -57,5 +65,35 @@ class Demonetize extends Cli\Controller implements Interfaces\CliControllerInter
                 ->storeUserSettings(['plus_demonetized' => true]);
             $this->out('[Demonetize CLI] User demonetized');
         }
+    }
+
+    /**
+     * Demonetize a post.
+     * @example
+     * - php cli.php Demonetize post --guid='1470454736242085891'
+     * - php cli.php Demonetize post --guid='1470454736242085891'
+     * @return void
+     */
+    public function post(): void
+    {
+        $guid = $this->getOpt('guid') ?? null;
+
+        $entity = $this->entitiesBuilder->single($guid);
+        if (!($entity instanceof DemonetizableEntityInterface)) {
+            $this->out('[Demonetize CLI] Invalid entity');
+            return;
+        }
+        
+        try {
+            ACL::$ignore = true;
+            $this->demonetizationContext->withStrategy($this->demonetizePostStrategy)
+                ->execute($entity);
+        } catch (\Exception $e) {
+            $this->out('[Demonetize CLI] An error has occurred');
+        } finally {
+            ACL::$ignore = false;
+        }
+
+        $this->out('[Demonetize CLI] Demonetized entity');
     }
 }
