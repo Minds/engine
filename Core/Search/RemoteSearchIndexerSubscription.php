c8c1c6c9adcbb1ea10de31880001a9e9422552da
Zack Wynne <zack@minds.com>
2023-03-08 11:21:16 -0700

    (feat): Adding RemoteSearchIndexerSubscription class

---
+++ Core/Search/RemoteSearchIndexerSubscription.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * This subscription will listen for changes in entities (create, update, delete) and update elasticsearch
+ * You can test by running `php cli.php EventStreams --subscription=Core\\Search\\SearchIndexerSubscription`
+ */
+namespace Minds\Core\Search;
+
+use Minds\Common\Urn;
+use Minds\Core\Blogs\Blog;
+use Minds\Core\Di\Di;
+use Minds\Core\Entities\Ops\EntitiesOpsEvent;
+use Minds\Core\Entities\Ops\EntitiesOpsTopic;
+use Minds\Core\Entities\Resolver;
+use Minds\Core\EventStreams\EventInterface;
+use Minds\Core\EventStreams\SubscriptionInterface;
+use Minds\Core\EventStreams\Topics\TopicInterface;
+use Minds\Core\Search\SearchIndexerSubscription as SearchSearchIndexerSubscription;
+use Minds\Entities\Activity;
+use Minds\Entities\Group;
+use Minds\Entities\Image;
+use Minds\Entities\User;
+use Minds\Entities\Video;
+
+class RemoteSearchIndexerSubscription extends SearchSearchIndexerSubscription
+{
+   
+    /**
+     * @return string
+     */
+    public function getSubscriptionId(): string
+    {
+        return 'remote-search-indexer';
+    }
+}
