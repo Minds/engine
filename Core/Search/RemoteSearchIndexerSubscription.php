c6f9c9bca43e27bd4b2be67d5e21a6e9873918a7
Zack Wynne <zack@minds.com>
2023-03-08 22:29:19 -0700

    (feat): cleanup remote search indexer subscription

--- Core/Search/RemoteSearchIndexerSubscription.php
+++ Core/Search/RemoteSearchIndexerSubscription.php
@@ -1,25 +1,11 @@
 <?php
 /**
  * This subscription will listen for changes in entities (create, update, delete) and update elasticsearch
- * You can test by running `php cli.php EventStreams --subscription=Core\\Search\\SearchIndexerSubscription`
+ * You can test by running `php cli.php EventStreams --subscription=Core\\Search\\RemoteSearchIndexerSubscription`
  */
 namespace Minds\Core\Search;
 
-use Minds\Common\Urn;
-use Minds\Core\Blogs\Blog;
-use Minds\Core\Di\Di;
-use Minds\Core\Entities\Ops\EntitiesOpsEvent;
-use Minds\Core\Entities\Ops\EntitiesOpsTopic;
-use Minds\Core\Entities\Resolver;
-use Minds\Core\EventStreams\EventInterface;
-use Minds\Core\EventStreams\SubscriptionInterface;
-use Minds\Core\EventStreams\Topics\TopicInterface;
 use Minds\Core\Search\SearchIndexerSubscription as SearchSearchIndexerSubscription;
-use Minds\Entities\Activity;
-use Minds\Entities\Group;
-use Minds\Entities\Image;
-use Minds\Entities\User;
-use Minds\Entities\Video;
 
 class RemoteSearchIndexerSubscription extends SearchSearchIndexerSubscription
 {
