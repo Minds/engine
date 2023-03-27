d466f4e207f3a89715e7809e4edb3b528c7510e6
Ben Hayward <ben@minds.com>
2023-03-27 12:59:38 +0100

    Skip injections of null Boosts in suggestions minds#3876

--- Spec/Core/Recommendations/Injectors/BoostSuggestionInjectorSpec.php
+++ Spec/Core/Recommendations/Injectors/BoostSuggestionInjectorSpec.php
@@ -2,9 +2,15 @@
 
 namespace Spec\Minds\Core\Recommendations\Injectors;
 
+use Minds\Common\Repository\Response;
+use Minds\Core\Boost\V3\Enums\BoostStatus;
+use Minds\Core\Boost\V3\Enums\BoostTargetAudiences;
+use Minds\Core\Boost\V3\Enums\BoostTargetLocation;
 use Minds\Core\Boost\V3\Manager as BoostManager;
+use Minds\Core\Boost\V3\Models\Boost;
 use Minds\Core\Log\Logger;
 use Minds\Core\Recommendations\Injectors\BoostSuggestionInjector;
+use Minds\Entities\User;
 use PhpSpec\ObjectBehavior;
 use PhpSpec\Wrapper\Collaborator;
 
@@ -25,5 +31,114 @@ class BoostSuggestionInjectorSpec extends ObjectBehavior
     public function it_is_initializable()
     {
         $this->shouldHaveType(BoostSuggestionInjector::class);
+    }
+
+    public function it_should_inject_a_boost(
+        Response $response,
+        User $targetUser,
+        User $suggestedUser1,
+        User $suggestedUser2,
+        User $suggestedUser3,
+        Response $boostRepoResponse,
+        Boost $boostedUser,
+        User $boostedUserEntity
+    ): void {
+        $response->toArray()
+            ->shouldBeCalled()
+            ->willReturn([
+                $suggestedUser1,
+                $suggestedUser2,
+                $suggestedUser3
+            ]);
+
+        $targetUser->getBoostRating()
+            ->shouldBeCalled()
+            ->willReturn(BoostTargetAudiences::SAFE);
+
+        $boostedUser->getGuid()
+            ->shouldBeCalled()
+            ->willReturn('123');
+
+        $boostedUserEntity->getType()
+            ->shouldBeCalled()
+            ->willReturn('user');
+
+        $boostedUser->getEntity()
+            ->shouldBeCalled()
+            ->willReturn($boostedUserEntity);
+
+        $boostRepoResponse->first()
+            ->shouldBeCalled()
+            ->willReturn($boostedUser);
+
+        $this->boostManager->getBoosts(
+            1,
+            0,
+            BoostStatus::APPROVED,
+            false,
+            null,
+            true,
+            BoostTargetAudiences::SAFE,
+            BoostTargetLocation::SIDEBAR,
+            null,
+            null
+        )
+            ->shouldBeCalled()
+            ->willReturn($boostRepoResponse);
+
+        $this->inject($response, $targetUser, 1);
+    }
+
+    public function it_should_NOT_inject_a_null_boost(
+        Response $response,
+        User $targetUser,
+        User $suggestedUser1,
+        User $suggestedUser2,
+        User $suggestedUser3,
+        Response $boostRepoResponse,
+        Boost $boostedUser,
+        User $boostedUserEntity
+    ): void {
+        $response->toArray()
+            ->shouldBeCalled()
+            ->willReturn([
+                $suggestedUser1,
+                $suggestedUser2,
+                $suggestedUser3
+            ]);
+
+        $targetUser->getBoostRating()
+            ->shouldBeCalled()
+            ->willReturn(BoostTargetAudiences::SAFE);
+
+        $boostedUser->getGuid()
+            ->shouldNotBeCalled();
+
+        $boostedUserEntity->getType()
+            ->shouldNotBeCalled();
+
+        $boostedUser->getEntity()
+            ->shouldNotBeCalled();
+
+        $boostRepoResponse->first()
+            ->shouldBeCalled()
+            ->willReturn(null);
+
+        $this->boostManager->getBoosts(
+            1,
+            0,
+            BoostStatus::APPROVED,
+            false,
+            null,
+            true,
+            BoostTargetAudiences::SAFE,
+            BoostTargetLocation::SIDEBAR,
+            null,
+            null
+        )
+            ->shouldBeCalled()
+            ->willReturn($boostRepoResponse);
+
+        $this->inject($response, $targetUser, 1);
     }
 }
