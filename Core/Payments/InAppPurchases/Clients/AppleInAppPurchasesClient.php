ddd192b0354dabd8d717ecec0a518d1486558e43
Mark Harding <mark@minds.com>
2023-03-28 14:00:02 +0100

    (fix): static analysis failure

--- Core/Payments/InAppPurchases/Clients/AppleInAppPurchasesClient.php
+++ Core/Payments/InAppPurchases/Clients/AppleInAppPurchasesClient.php
@@ -3,6 +3,15 @@ declare(strict_types=1);
 
 namespace Minds\Core\Payments\InAppPurchases\Clients;
 
+use Minds\Core\Payments\InAppPurchases\Models\InAppPurchase;
+
 class AppleInAppPurchasesClient implements InAppPurchaseClientInterface
 {
+    /**
+     * TODO
+     */
+    public function acknowledgePurchase(InAppPurchase $inAppPurchase): bool
+    {
+        return false;
+    }
 }
