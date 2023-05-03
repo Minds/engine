67eb4fa521cf63204112a1cdc592488118e1ce4f
Fausto Arcidiacono <fausto.arcidiacono@gmail.com>
2023-05-03 07:26:52 +0000

    (fix): Fixed boost payment refund when Vitess exception triggered minds/engine#2555

--- Core/Boost/V3/Exceptions/BoostPaymentSetupFailedException.php
+++ Core/Boost/V3/Exceptions/BoostPaymentSetupFailedException.php
@@ -6,5 +6,5 @@ namespace Minds\Core\Boost\V3\Exceptions;
 class BoostPaymentSetupFailedException extends \Minds\Exceptions\ServerErrorException
 {
     protected $code = 500;
-    protected $message = "";
+    protected $message = "There was an error processing the payment";
 }
