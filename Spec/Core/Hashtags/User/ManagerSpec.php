cb7a2ecd8978d8b7b01dff7fc3e2b4387729ca4c
Ben Hayward <ben@minds.com>
2023-05-10 10:37:16 +0100

    Unneeded change

--- Spec/Core/Hashtags/User/ManagerSpec.php
+++ Spec/Core/Hashtags/User/ManagerSpec.php
@@ -2,27 +2,14 @@
 
 namespace Spec\Minds\Core\Hashtags\User;
 
-use Minds\Common\PseudonymousIdentifier;
-use Minds\Core\Hashtags\User\PseudoHashtags;
-use Minds\Core\Hashtags\HashtagEntity;
-use Minds\Core\Data\Cassandra;
+use Minds\Core\Hashtags\User\Manager;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
-class PseudoHashtagsSpec extends ObjectBehavior
+class ManagerSpec extends ObjectBehavior
 {
-    private $db;
-    private $pseuodId;
-
-    public function let(Cassandra\Client $db, PseudonymousIdentifier $pseudoId)
-    {
-        $this->beConstructedWith($db, $pseudoId);
-        $this->db = $db;
-        $this->pseuodId = $pseudoId;
-    }
-
     public function it_is_initializable()
     {
-        $this->shouldHaveType(PseudoHashtags::class);
+        $this->shouldHaveType(Manager::class);
     }
 }
