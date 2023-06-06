0614aadde9386a6c41922a192c17a3e83a4c2a30
Fausto Arcidiacono <fausto.arcidiacono@gmail.com>
2023-06-06 10:36:53 +0100

    (chore): Amended consname to DEFAULT_GUID

--- Common/SystemUser.php
+++ Common/SystemUser.php
@@ -7,7 +7,7 @@ use Minds\Entities\User;
 class SystemUser extends User
 {
     /** @var string */
-    const GUID = '100000000000000519';
+    const DEFAULT_GUID = '100000000000000519';
 
     public function __construct()
     {
@@ -25,7 +25,7 @@ class SystemUser extends User
      */
     public function getGUID(): string
     {
-        return $this->guid ?: self::GUID;
+        return $this->guid ?: self::DEFAULT_GUID;
     }
 
     /**
