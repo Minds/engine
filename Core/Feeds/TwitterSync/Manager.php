bb861fa23907330057ea0dc5f9f52c44c87e8e92
Ben Hayward <ben@minds.com>
2023-06-09 17:48:14 +0100

    Update for feedback

--- Core/Feeds/TwitterSync/Manager.php
+++ Core/Feeds/TwitterSync/Manager.php
@@ -79,7 +79,7 @@ class Manager
             foreach ($latestTweets as $tweet) {
                 if (array_filter($tweet->getUrls(), function ($tweetUrl) use ($verificationString) {
                     return strtolower($tweetUrl) === $verificationString;
-                }) || strpos($tweet->getText(), $verificationString) !== false) {
+                }) || str_contains($tweet->getText(), $verificationString)) {
                     $verifiedTweetId = $tweet->getId();
                     break; // Success!
                 }
