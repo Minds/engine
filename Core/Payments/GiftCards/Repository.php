73d3d9cacc2c370615466db9687844bc42711ec3
Fausto Arcidiacono <fausto.arcidiacono@gmail.com>
2023-08-11 10:46:46 +0100

    (fix): Removed unnecessary code

--- Core/Payments/GiftCards/Repository.php
+++ Core/Payments/GiftCards/Repository.php
@@ -623,29 +623,4 @@ class Repository extends AbstractRepository
         $this->mysqlHandler->bindValuesToPreparedStatement($statement, $values);
         $statement->execute();
     }
-
-    /**
-     * @param int $giftCardGuid
-     * @param int $expiresAt
-     * @return void
-     * @throws ServerErrorException
-     */
-    public function updateGiftCardExpiry(int $giftCardGuid, int $expiresAt): void
-    {
-        $statement = $this->mysqlClientWriterHandler->update()
-            ->table('minds_gift_cards')
-            ->set([
-                'expires_at' => new RawExp(':expires_at'),
-            ])
-            ->where('guid', Operator::EQ, new RawExp(':guid'))
-            ->prepare();
-        $values = [
-            'expires_at' => date('c', $expiresAt),
-            'guid' => $giftCardGuid,
-        ];
-
-        $this->mysqlHandler->bindValuesToPreparedStatement($statement, $values);
-
-        $statement->execute();
-    }
 }
