6b38231dd5291da2accc427eca426948c6c84c8d
Fausto Arcidiacono <fausto.arcidiacono@gmail.com>
2023-08-17 17:39:24 +0100

    (fix): Fixed issue with top activity post injector causing cursor error

--- Core/Feeds/GraphQL/Controllers/NewsfeedController.php
+++ Core/Feeds/GraphQL/Controllers/NewsfeedController.php
@@ -155,7 +155,7 @@ class NewsfeedController
             default => throw new UserError("Invalid algorithm supplied")
         };
 
-        if ($algorithm === NewsfeedAlgorithmsEnum::FORYOU && $this->experimentsManager->setUser($loggedInUser)->isOn('minds-4169-for-you-top-posts-injection')) {
+        if ($algorithm === NewsfeedAlgorithmsEnum::FORYOU && !$after && $this->experimentsManager->setUser($loggedInUser)->isOn('minds-4169-for-you-top-posts-injection')) {
             $topQueryOpts = new QueryOpts(
                 limit: 1, // TODO: swap with configurable value
                 query: "",
@@ -197,7 +197,17 @@ class NewsfeedController
                 break;
             }
 
-            if ($i === 0) { // Priority notice is always at the top
+            if (
+                (
+                    $this->isForYouTopExperimentActive($loggedInUser, $algorithm, $after) &&
+                    $i === 1
+                )
+                ||
+                (
+                    !$this->isForYouTopExperimentActive($loggedInUser, $algorithm, $after) &&
+                    $i === 0
+                )
+            ) { // Priority notice is always at the top
                 $priorityNotices = $this->buildInFeedNotices(
                     loggedInUser: $loggedInUser,
                     cursor: $cursor,
@@ -292,7 +302,7 @@ class NewsfeedController
 
             $showExplicitVoteButtons = $this->showExplicitVoteButtons($activity, $loggedInUser, $i);
 
-            $edges[] = new ActivityEdge($activity, $cursor, $showExplicitVoteButtons);
+            $edges[] = new ActivityEdge($activity, $cursor ?? "", $showExplicitVoteButtons);
         }
 
         if (empty($edges)) {
@@ -412,6 +422,14 @@ class NewsfeedController
         }
 
         return $edges;
+    }
+
+    private function isForYouTopExperimentActive(User $loggedInUser, NewsfeedAlgorithmsEnum $algorithm, ?string $after): bool
+    {
+        return
+            $algorithm === NewsfeedAlgorithmsEnum::FORYOU &&
+            !$after &&
+            $this->experimentsManager->setUser($loggedInUser)->isOn('minds-4169-for-you-top-posts-injection');
     }
 
     /**
