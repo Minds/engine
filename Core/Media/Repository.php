7ef6a281f6a751638a83e052da5834dac2101896
Mark Harding <mark@minds.com>
2023-11-13 13:19:12 +0000

    (fix): media entities repository should use entities builder

--- Core/Media/Repository.php
+++ Core/Media/Repository.php
@@ -2,18 +2,30 @@
 namespace Minds\Core\Media;
 
 use Minds\Core;
+use Minds\Core\Di\Di;
+use Minds\Core\Entities\Actions\Delete;
+use Minds\Core\EntitiesBuilder;
 use Minds\Core\Security;
 use Minds\Entities;
 
 class Repository
 {
+    public function __construct(
+        private ?EntitiesBuilder $entitiesBuilder = null,
+        private ?Delete $deleteAction = null,
+    )
+    {
+        $this->entitiesBuilder ??= Di::_()->get(EntitiesBuilder::class);
+        $this->deleteAction ??= new Delete();
+    }
+
     public function getEntity($guid)
     {
         if (!$guid) {
             return false;
         }
 
-        $entity = Core\Entities::build(new Entities\Entity($guid));
+        $entity = $this->entitiesBuilder->single($guid);
 
         if (!$entity || !$entity->guid || !Security\ACL::_()->read($entity)) {
             return false;
@@ -28,13 +40,13 @@ class Repository
             return false;
         }
 
-        $entity = Entities\Factory::build($guid);
+        $entity = $this->entitiesBuilder->single($guid);
 
         if (!$entity || !$entity->guid || !$entity->canEdit()) {
             return false;
         }
 
-        $entity->delete();
+        $this->deleteAction->setEntity($entity)->delete();
 
         return true;
     }
