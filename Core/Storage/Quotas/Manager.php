bec00cc46fc2e8d4b4108ecd4fa7b2db2dda8a21
Fausto Arcidiacono <fausto.arcidiacono@gmail.com>
2023-11-23 12:12:52 +0000

    (feat): Changed setting used to connect to OCI storage

--- Core/Storage/Quotas/Manager.php
+++ Core/Storage/Quotas/Manager.php
@@ -104,7 +104,7 @@ class Manager
 
         $response = $this->ociObjectStorageClient
             ->headObject([
-                'namespaceName' => $this->config->get('storage')['oci_namespace'],
+                'namespaceName' => $this->config->get('oci')['api_auth']['bucket_namespace'],
                 'bucketName' => $this->config->get('storage')['oci_bucket_name'],
                 'objectName' => $key
             ]);
