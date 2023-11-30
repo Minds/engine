1839f17e38adb44c3da4b4738f85c8b42e6a2928
Ben Hayward <ben@minds.com>
2023-11-30 14:53:26 +0000

    Fix logic

--- Controllers/api/v2/search/suggest.php
+++ Controllers/api/v2/search/suggest.php
@@ -41,7 +41,7 @@ class suggest implements Interfaces\Api, Interfaces\ApiIgnorePam
 
         // Rather than throwing an error, we are just defaulting to user to avoid potential
         // breaking changes across clients following this addition.
-        if ($entityType !== 'user' || $entityType !== 'group') {
+        if (!in_array($entityType, ['user', 'group'])) {
             $entityType = 'user';
         }
 
