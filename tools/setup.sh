0355805b32d251435e8062c6cfb5cdb731c76001
Ben <ben@minds.com>
2024-02-08 19:22:54 +0000

    Fix build pipeline by updating composer hash https://gitlab.com/minds/engine/-/issues/2708

--- tools/setup.sh
+++ tools/setup.sh
@@ -17,7 +17,7 @@ CURRENT_DIR=`pwd`
 # Setup composer
 # Hash update information - https://getcomposer.org/download/
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('sha384', 'composer-setup.php') === 'e21205b207c3ff031906575712edab6f13eb0b361f2085f1f1237b7126d785e826a450292b6cfd1d64d92e6563bbde02') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php -r "if (hash_file('sha384', 'composer-setup.php') === 'edb40769019ccf227279e3bdd1f5b2e9950eb000c3233ee85148944e555d97be3ea4f40c3c2fe73b22f875385f6a5155') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 php composer-setup.php
 php -r "unlink('composer-setup.php');"
 
