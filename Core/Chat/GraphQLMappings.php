bdc14e4e068365f855a6a920804dd51e91aa29aa
Mark Harding <mark@minds.com>
2024-04-02 14:38:18 +0100

    (fix): use a static list for types

--- Core/Chat/GraphQLMappings.php
+++ Core/Chat/GraphQLMappings.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace Minds\Core\Chat;
 
 use Minds\Core\GraphQL\AbstractGraphQLMappings;
+use TheCodingMachine\GraphQLite\Mappers\StaticClassListTypeMapperFactory;
 
 class GraphQLMappings extends AbstractGraphQLMappings
 {
@@ -11,6 +12,18 @@ class GraphQLMappings extends AbstractGraphQLMappings
     {
         $this->schemaFactory->addControllerNamespace('Minds\Core\Chat\\Controllers');
         $this->schemaFactory->addTypeNamespace('Minds\\Core\\Chat\\Enums');
-        $this->schemaFactory->addTypeNamespace('Minds\Core\Chat\Types');
+        $this->schemaFactory->addTypeMapperFactory(new StaticClassListTypeMapperFactory([
+            // Messages
+            Types\ChatMessageEdge::class,
+            Types\ChatMessageNode::class,
+            Types\ChatMessagesConnection::class,
+            // Rooms
+            Types\ChatRoomEdge::class,
+            Types\ChatRoomNode::class,
+            Types\ChatRoomsConnection::class,
+            // Room members
+            Types\ChatRoomMemberEdge::class,
+            Types\ChatRoomMembersConnection::class,
+        ]));
     }
 }
