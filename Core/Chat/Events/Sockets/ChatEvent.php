b35150c1aedc684f4ba41053558b7847128907a7
Fausto Arcidiacono <fausto.arcidiacono@gmail.com>
2024-04-03 14:23:43 +0100

    Added serialization to chat event class

--- Core/Chat/Events/Sockets/ChatEvent.php
+++ Core/Chat/Events/Sockets/ChatEvent.php
@@ -3,13 +3,22 @@ declare(strict_types=1);
 
 namespace Minds\Core\Chat\Events\Sockets;
 
+use JsonSerializable;
 use Minds\Core\Chat\Events\Sockets\Enums\ChatEventTypeEnum;
 
-class ChatEvent
+class ChatEvent implements JsonSerializable
 {
     public function __construct(
         public ChatEventTypeEnum $type,
         public array $metadata = []
     ) {
+    }
+
+    public function jsonSerialize(): array
+    {
+        return [
+            'type' => $this->type,
+            'metadata' => $this->metadata,
+        ];
     }
 }
