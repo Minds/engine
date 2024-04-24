421d6917b63758adddd36060ab4c8e3e06c08afe
Fausto Arcidiacono <fausto@novasoft-solutions.co.uk>
2024-04-24 16:54:11 +0100

    Changes for PHP 8.2

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ use Zend\Diactoros\ServerRequestFactory;
 
 require_once(dirname(__FILE__) . "/start.php");
 
-error_reporting(E_ALL);
+error_reporting(E_ALL & ~E_DEPRECATED & ~E_NOTICE);
 
 $router = new Minds\Core\Router();
 $router->route(ServerRequestFactory::fromGlobals());
