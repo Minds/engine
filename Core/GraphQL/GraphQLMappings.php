b120d9f0bcadf053a1d89e72416261684bb98aa1
Mark Harding <mark@minds.com>
2024-05-09 13:23:16 +0100

    (fix): Router enums not working

--- Core/GraphQL/GraphQLMappings.php
+++ Core/GraphQL/GraphQLMappings.php
@@ -2,15 +2,17 @@
 
 namespace Minds\Core\GraphQL;
 
+use Minds\Core\Router\Enums\ApiScopeEnum;
 use TheCodingMachine\GraphQLite\Mappers\StaticClassListTypeMapperFactory;
 
 class GraphQLMappings extends AbstractGraphQLMappings
 {
     public function register(): void
     {
+        $this->schemaFactory->addTypeNamespace("Minds\\Core\\Router\\Enums");
         $this->schemaFactory->addTypeMapperFactory(new StaticClassListTypeMapperFactory([
             Types\KeyValuePair::class,
-            Types\KeyValueType::class
+            Types\KeyValueType::class,
         ]));
     }
 }
