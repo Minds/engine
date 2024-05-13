35ca1f77b98b41ddb9c1da78da666622d752e734
Ben <ben@minds.com>
2024-05-13 11:37:51 +0100

    Add monographic icon for tenant app config minds#4846

--- Core/MultiTenant/MobileConfigs/Services/MobileConfigAssetsService.php
+++ Core/MultiTenant/MobileConfigs/Services/MobileConfigAssetsService.php
@@ -87,6 +87,7 @@ class MobileConfigAssetsService
                 MobileConfigImageTypeEnum::SPLASH => 'default-square-logo.png',
                 MobileConfigImageTypeEnum::SQUARE_LOGO => 'default-square-logo.png',
                 MobileConfigImageTypeEnum::HORIZONTAL_LOGO => 'default-horizontal-logo.png',
+                MobileConfigImageTypeEnum::MONOGRAPHIC_ICON => 'default-monographic-icon.png',
             };
             $filepath = $this->config->get('path') . "engine/Assets/tenant/$fileName";
             $contents = file_get_contents($filepath);
