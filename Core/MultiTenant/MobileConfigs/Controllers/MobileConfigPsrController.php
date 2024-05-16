5d8590f4790a63f04dc46faf25fab4357d4dcbc3
Ben <ben@minds.com>
2024-05-16 12:29:03 +0000

    [engine] Monographic icon for mobile network app config minds#4846

--- Core/MultiTenant/MobileConfigs/Controllers/MobileConfigPsrController.php
+++ Core/MultiTenant/MobileConfigs/Controllers/MobileConfigPsrController.php
@@ -33,11 +33,6 @@ class MobileConfigPsrController
             throw new UserErrorException('A valid type must be provided');
         }
 
-        // Temporary work around
-        if ($type === MobileConfigImageTypeEnum::ICON_MONO) {
-            $type = MobileConfigImageTypeEnum::ICON;
-        }
-
         $file = $this->mobileConfigAssetsService->getImageFileByType($type);
         $file->open('read');
 
