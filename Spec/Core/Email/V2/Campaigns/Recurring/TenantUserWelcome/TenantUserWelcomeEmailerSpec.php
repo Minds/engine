0a9e0cbd9f6c4975a36501a779eaa4dc3ead30cd
Ben <ben@minds.com>
2024-08-27 12:44:25 +0000

    [engine] Prevent membership options during onboarding for members minds#5060

--- Spec/Core/Email/V2/Campaigns/Recurring/TenantUserWelcome/TenantUserWelcomeEmailerSpec.php
+++ Spec/Core/Email/V2/Campaigns/Recurring/TenantUserWelcome/TenantUserWelcomeEmailerSpec.php
@@ -17,6 +17,7 @@ use Minds\Core\MultiTenant\Types\FeaturedGroup;
 use Minds\Core\Payments\SiteMemberships\Enums\SiteMembershipBillingPeriodEnum;
 use Minds\Core\Payments\SiteMemberships\Enums\SiteMembershipPricingModelEnum;
 use Minds\Core\Payments\SiteMemberships\Services\SiteMembershipReaderService;
+use Minds\Core\Payments\SiteMemberships\Services\SiteMembershipSubscriptionsService;
 use Minds\Core\Payments\SiteMemberships\Types\SiteMembership;
 use Minds\Entities\User;
 use PhpSpec\ObjectBehavior;
@@ -30,6 +31,7 @@ class TenantUserWelcomeEmailerSpec extends ObjectBehavior
     protected Collaborator $configMock;
     protected Collaborator $tenantTemplateVariableInjectorMock;
     protected Collaborator $siteMembershipReaderServiceMock;
+    protected Collaborator $siteMembershipSubscriptionsService;
     protected Collaborator $featuredEntityServiceMock;
     protected Collaborator $managerMock;
 
@@ -39,6 +41,7 @@ class TenantUserWelcomeEmailerSpec extends ObjectBehavior
         Config $config,
         TenantTemplateVariableInjector $tenantTemplateVariableInjector,
         SiteMembershipReaderService $siteMembershipReaderService,
+        SiteMembershipSubscriptionsService $siteMembershipSubscriptionsService,
         FeaturedEntityService $featuredEntityService,
         Manager $manager,
     ) {
@@ -47,6 +50,7 @@ class TenantUserWelcomeEmailerSpec extends ObjectBehavior
         $this->configMock = $config;
         $this->tenantTemplateVariableInjectorMock = $tenantTemplateVariableInjector;
         $this->siteMembershipReaderServiceMock = $siteMembershipReaderService;
+        $this->siteMembershipSubscriptionsService = $siteMembershipSubscriptionsService;
         $this->featuredEntityServiceMock = $featuredEntityService;
         $this->managerMock = $manager;
 
@@ -56,6 +60,7 @@ class TenantUserWelcomeEmailerSpec extends ObjectBehavior
             $this->configMock,
             $this->tenantTemplateVariableInjectorMock,
             $this->siteMembershipReaderServiceMock,
+            $this->siteMembershipSubscriptionsService,
             $this->featuredEntityServiceMock,
             $this->managerMock
         );
@@ -106,6 +111,9 @@ class TenantUserWelcomeEmailerSpec extends ObjectBehavior
             ->shouldBeCalled()
             ->willReturn($siteName);
 
+        $this->templateMock->clear()
+          ->shouldBeCalled();
+
         $this->templateMock->setTemplate('default.v2.tpl')
           ->shouldBeCalled();
 
@@ -152,7 +160,13 @@ class TenantUserWelcomeEmailerSpec extends ObjectBehavior
 
         $this->templateMock->set('actionButton', Argument::any())
           ->shouldBeCalled();
-      
+     
+        $this->siteMembershipSubscriptionsService->hasActiveSiteMembershipSubscription(
+            user: $user
+        )
+          ->shouldBeCalled()
+          ->willReturn(false);
+
         $this->siteMembershipReaderServiceMock->getSiteMemberships()
           ->shouldBeCalled()
           ->willReturn($siteMemberships);
@@ -238,6 +252,9 @@ class TenantUserWelcomeEmailerSpec extends ObjectBehavior
             ->shouldBeCalled()
             ->willReturn($siteName);
   
+        $this->templateMock->clear()
+          ->shouldBeCalled();
+
         $this->templateMock->setTemplate('default.v2.tpl')
           ->shouldBeCalled();
   
@@ -284,7 +301,13 @@ class TenantUserWelcomeEmailerSpec extends ObjectBehavior
   
         $this->templateMock->set('actionButton', Argument::any())
           ->shouldBeCalled();
-        
+
+        $this->siteMembershipSubscriptionsService->hasActiveSiteMembershipSubscription(
+            user: $user
+        )
+          ->shouldBeCalled()
+          ->willReturn(false);
+
         $this->siteMembershipReaderServiceMock->getSiteMemberships()
           ->shouldBeCalled()
           ->willReturn($siteMemberships);
@@ -316,6 +339,151 @@ class TenantUserWelcomeEmailerSpec extends ObjectBehavior
           ->shouldBeCalled()
           ->willReturn($siteUrl);
   
+        $this->build();
+    }
+
+    public function it_should_build_with_no_memberships_when_user_has_an_active_membership(
+        User $user,
+        FeaturedEntityConnection $featuredEntityConnection,
+        FeaturedEntityEdge $featuredEntityEdge
+    ): void {
+        $siteName = 'Testnet';
+        $siteUrl = 'https://example.minds.com/';
+        $username = 'testuser';
+      
+        $email = 'noreply@minds.com';
+        $tenantId = 123;
+        $userGuid = Guid::build();
+        $siteMemberships = [new SiteMembership(
+            membershipGuid: Guid::build(),
+            membershipName: 'Test Membership',
+            membershipPriceInCents: 999,
+            membershipBillingPeriod: SiteMembershipBillingPeriodEnum::MONTHLY,
+            membershipPricingModel: SiteMembershipPricingModelEnum::RECURRING,
+            membershipDescription: 'Test Membership Description',
+        )];
+      
+        $featuredEntityEdge->getNode()
+          ->shouldBeCalled()
+          ->willReturn(new FeaturedGroup(
+              tenantId: 123,
+              entityGuid: Guid::build(),
+              autoSubscribe: false,
+              recommended: false,
+              autoPostSubscription: false,
+              name: 'Test Group',
+              briefDescription: 'Test Group Description',
+              membersCount: 3
+          ));
+      
+        $featuredEntityConnection->getEdges()
+          ->shouldBeCalled()
+          ->willReturn([$featuredEntityEdge]);
+
+        $user->username = $username;
+
+        $user->get('username')
+          ->shouldBeCalled($username);
+
+        $user->get('name')
+          ->shouldBeCalled($username);
+
+        $user->getEmail()
+          ->shouldBeCalled()
+          ->willReturn($email);
+
+        $user->getGUID()
+          ->shouldBeCalled()
+          ->willReturn($userGuid);
+
+        $this->setUser($user);
+
+        $this->configMock->get('site_name')
+            ->shouldBeCalled()
+            ->willReturn($siteName);
+
+        $this->templateMock->clear()
+          ->shouldBeCalled();
+
+        $this->templateMock->setTemplate('default.v2.tpl')
+          ->shouldBeCalled();
+
+        $this->templateMock->setBody('./template.tpl')
+          ->shouldBeCalled();
+
+        $this->templateMock->set('headerText', "Welcome!")
+          ->shouldBeCalled();
+
+        $this->templateMock->set('bodyText', "Thanks for joining $siteName. Here's what you can do next to get the most out of the community.")
+          ->shouldBeCalled();
+
+        $this->templateMock->set('preheader', "Thanks for joining $siteName")
+          ->shouldBeCalled();
+
+        $this->templateMock->set('user', $user)
+          ->shouldBeCalled();
+
+        $this->templateMock->set('username', Argument::any())
+          ->shouldBeCalled();
+
+        $this->templateMock->set('email', $email)
+          ->shouldBeCalled();
+
+        $this->templateMock->set('guid', $userGuid)
+          ->shouldBeCalled();
+
+        $this->templateMock->set('campaign', 'with')
+          ->shouldBeCalled();
+
+        $this->templateMock->set('topic', 'welcome')
+          ->shouldBeCalled();
+
+        $this->templateMock->set('tracking', Argument::type('string'))
+          ->shouldBeCalled();
+
+        $this->configMock->get('tenant_id')
+          ->shouldBeCalled()
+          ->willReturn($tenantId);
+
+        $this->tenantTemplateVariableInjectorMock->inject($this->templateMock)
+          ->shouldBeCalled()
+          ->willReturn($this->templateMock);
+
+        $this->templateMock->set('actionButton', Argument::any())
+          ->shouldBeCalled();
+
+        $this->siteMembershipSubscriptionsService->hasActiveSiteMembershipSubscription(
+            user: $user
+        )
+          ->shouldBeCalled()
+          ->willReturn(true);
+
+        $this->siteMembershipReaderServiceMock->getSiteMemberships()
+          ->shouldNotBeCalled();
+
+        $this->templateMock->set('site_membership_containers', [])
+          ->shouldBeCalled();
+
+        $this->featuredEntityServiceMock->getFeaturedEntities(
+            type: FeaturedEntityTypeEnum::GROUP,
+            loadAfter: 0,
+            limit: 3
+        )
+          ->shouldBeCalled()
+          ->willReturn($featuredEntityConnection);
+
+        $this->templateMock->set('featured_group_containers', Argument::that(function ($featuredGroups) {
+            return $featuredGroups[0]['name'] === 'Test Group'
+              && $featuredGroups[0]['description'] === 'Test Group Description'
+              && str_starts_with($featuredGroups[0]['avatar_url'], 'https://example.minds.com/fs/v1/avatars')
+              && str_starts_with($featuredGroups[0]['join_url'], 'https://example.minds.com/group/');
+        }))
+          ->shouldBeCalled();
+
+        $this->configMock->get('site_url')
+          ->shouldBeCalled()
+          ->willReturn($siteUrl);
+
         $this->build();
     }
 
@@ -385,6 +553,9 @@ class TenantUserWelcomeEmailerSpec extends ObjectBehavior
             ->shouldBeCalled()
             ->willReturn($siteName);
 
+        $this->templateMock->clear()
+            ->shouldBeCalled();
+
         $this->templateMock->setTemplate('default.v2.tpl')
           ->shouldBeCalled();
 
@@ -431,7 +602,13 @@ class TenantUserWelcomeEmailerSpec extends ObjectBehavior
 
         $this->templateMock->set('actionButton', Argument::any())
           ->shouldBeCalled();
-      
+
+        $this->siteMembershipSubscriptionsService->hasActiveSiteMembershipSubscription(
+            user: $user
+        )
+          ->shouldBeCalled()
+          ->willReturn(false);
+
         $this->siteMembershipReaderServiceMock->getSiteMemberships()
           ->shouldBeCalled()
           ->willReturn($siteMemberships);
@@ -562,6 +739,9 @@ class TenantUserWelcomeEmailerSpec extends ObjectBehavior
             ->shouldBeCalled()
             ->willReturn($siteName);
 
+        $this->templateMock->clear()
+          ->shouldBeCalled();
+
         $this->templateMock->setTemplate('default.v2.tpl')
           ->shouldBeCalled();
 
@@ -608,7 +788,13 @@ class TenantUserWelcomeEmailerSpec extends ObjectBehavior
 
         $this->templateMock->set('actionButton', Argument::any())
           ->shouldBeCalled();
-    
+
+        $this->siteMembershipSubscriptionsService->hasActiveSiteMembershipSubscription(
+            user: $user
+        )
+          ->shouldBeCalled()
+          ->willReturn(false);
+
         $this->siteMembershipReaderServiceMock->getSiteMemberships()
           ->shouldBeCalled()
           ->willReturn($siteMemberships);
