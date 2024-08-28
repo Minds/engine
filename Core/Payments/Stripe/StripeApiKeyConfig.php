7cfd46752075efeef21c7ee9e6ab3ee143094830
Mark Harding <mark@minds.com>
2024-08-28 11:27:55 +0100

    (fix): customer portal not working when using test mode - #2749

--- Core/Payments/Stripe/StripeApiKeyConfig.php
+++ Core/Payments/Stripe/StripeApiKeyConfig.php
@@ -44,7 +44,7 @@ class StripeApiKeyConfig
             $stripeConfig['api_key'] = $secKey;
         }
 
-        return $this->shouldUseTestMode($user, $stripeConfig) ?
+        return $this->shouldUseTestMode($user) ?
             $stripeConfig['test_api_key'] :
             $stripeConfig['api_key'];
     }
@@ -55,8 +55,13 @@ class StripeApiKeyConfig
      * @param string $testEmailSuffix - suffix for test email to be used in regex.
      * @return boolean true if a test key should be used.
      */
-    private function shouldUseTestMode(?User $user = null, $stripeConfig = []): bool
+    public function shouldUseTestMode(?User $user = null): bool
     {
+        if (!$user) {
+            $user = $this->activeSession->getUser();
+        }
+
+        $stripeConfig = $this->config->get('payments')['stripe'];
         $testEmailRegex = '/^[A-Za-z]+\+'.$stripeConfig['test_email'].'$/';
 
         return $user &&
