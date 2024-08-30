fe8298b61cdb3a44eb3e09b9b1b343693fa5d724
Ben <ben@minds.com>
2024-08-30 19:13:00 +0100

    Add comma

--- Spec/Core/Payments/SiteMemberships/PaywalledEntities/Services/PaywalledEntityServiceSpec.php
+++ Spec/Core/Payments/SiteMemberships/PaywalledEntities/Services/PaywalledEntityServiceSpec.php
@@ -24,7 +24,7 @@ class PaywalledEntityServiceSpec extends ObjectBehavior
     public function let(
         PaywalledEntitiesRepository $paywalledEntitiesRepositoryMock,
         SiteMembershipReaderService $siteMembershipReaderServiceMock,
-        CacheInterface $cacheMock
+        CacheInterface $cacheMock,
     ) {
         $this->beConstructedWith($paywalledEntitiesRepositoryMock, $siteMembershipReaderServiceMock, $cacheMock);
         $this->paywalledEntitiesRepositoryMock = $paywalledEntitiesRepositoryMock;
