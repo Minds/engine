d5e010698aecdeadf7eefed69aa4b5020ef5a042
Mark Harding <mark@minds.com>
2024-11-07 09:10:48 +0000

    (chore): revert file

--- Core/ActivityPub/Services/EmitActivityService.php
+++ Core/ActivityPub/Services/EmitActivityService.php
@@ -163,9 +163,6 @@ class EmitActivityService
             'https://bae.st/inbox', // Unresponsive
             'https://shitposter.club/inbox', // Unresponsive
             'https://poster.place/inbox', // Unresponsive
-            'https://detroitriotcity.com/inbox',
-            'https://social.fbxl.net/inbox',
-            'https://fuwafuwa.town/inbox',
         ];
 
         if (in_array($inboxUrl, $disallowInboxes, true) === true) {
