d8b918ec247972a470672c6b064e8ba97e116513
Ben <ben@minds.com>
2024-11-19 17:07:40 +0000

    Use GUID

--- Core/Comments/GraphQL/Controllers/PinnedCommentsController.php
+++ Core/Comments/GraphQL/Controllers/PinnedCommentsController.php
@@ -26,19 +26,19 @@ class PinnedCommentsController
 
     /**
      * Pins or unpins a comment.
-     * @param string $commentUrn - the URN of the comment to pin or unpin.
+     * @param string $commentGuid - the GUID of the comment to pin or unpin.
      * @param bool $pinned - whether to pin or unpin the comment.
      * @return bool - true if the comment was pinned or unpinned, false otherwise.
      */
     #[Mutation]
     #[Logged]
     public function commentPinnedState(
-        string $commentUrn,
+        string $commentGuid,
         bool $pinned,
         #[InjectUser] ?User $loggedInUser = null,
     ): bool {
         try {
-            $comment = $this->manager->getByUrn($commentUrn);
+            $comment = $this->manager->getByLuid($commentGuid);
 
             if (!$comment) {
                 throw new GraphQLException('Comment not found');
