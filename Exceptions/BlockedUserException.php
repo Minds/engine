c006cb07de485dd58f812fd419846eb16c85062b
Mark Harding <mark@minds.com>
2025-04-30 08:27:44 +0100

    (chore): Throw blocked exception as a 403

--- Exceptions/BlockedUserException.php
+++ Exceptions/BlockedUserException.php
@@ -1,9 +1,12 @@
 <?php
 namespace Minds\Exceptions;
 
+use Minds\Core\Router\Exceptions\ForbiddenException;
+
 /**
  * Exception thrown by trying to comment to a blocked user
  */
-class BlockedUserException extends \Exception
+class BlockedUserException extends ForbiddenException
 {
+    protected $message = 'The user may have blocked you from interacting with them.';
 }
