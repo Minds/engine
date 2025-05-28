c54266cf3834298e3c726d568df924fff50deea8
Mark Harding <mark@minds.com>
2025-05-28 13:34:24 +0100

    (fix): Do not allow excessive query size to be overloaded with graphql

--- Core/GraphQL/Controller.php
+++ Core/GraphQL/Controller.php
@@ -3,9 +3,11 @@ namespace Minds\Core\GraphQL;
 
 use GraphQL\GraphQL;
 use GraphQL\Type\Schema;
+use GraphQL\Validator\DocumentValidator;
 use Minds\Core\Di\Di;
 use Minds\Core\GraphQL\Services\AuthorizationService;
 use Minds\Core\GraphQL\Services\AuthService;
+use Minds\Core\Router\Exceptions\ForbiddenException;
 use Minds\Core\Security\Rbac\Services\RolesService;
 use Psr\Http\Message\ServerRequestInterface;
 use TheCodingMachine\GraphQLite\Context\Context;
@@ -20,10 +22,23 @@ class Controller
         $input = json_decode($rawInput, true);
         $query = $input['query'];
         $variableValues = isset($input['variables']) ? $input['variables'] : null;
+
+        // Excessive query sizes are not accepted
+        if (strlen($query) > 5000) {
+            throw new ForbiddenException();
+        }
   
         $schema = Di::_()->get(Schema::class);
 
-        $result = GraphQL::executeQuery($schema, $query, null, new Context(), $variableValues);
+        $result = GraphQL::executeQuery(
+            schema: $schema,
+            source: $query,
+            contextValue: new Context(),
+            variableValues: $variableValues,
+            validationRules: [
+                new \GraphQL\Validator\Rules\QueryDepth(15),
+            ]
+        );
         $output = $result->toArray();
 
         if ($result->errors) {
