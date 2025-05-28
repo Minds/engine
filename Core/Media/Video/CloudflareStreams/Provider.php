e72b032d6b64efbe033da83ff39226802c3ecf8a
Mark Harding <mark@minds.com>
2025-05-28 12:01:47 +0100

    (chore): limit query depth to 15 to prevent overloading

--- Core/Media/Video/CloudflareStreams/Provider.php
+++ Core/Media/Video/CloudflareStreams/Provider.php
@@ -4,6 +4,8 @@ namespace Minds\Core\Media\Video\CloudflareStreams;
 
 use Minds\Core\Di\Di;
 use Minds\Core\Di\Provider as DiProvider;
+use Minds\Core\EntitiesBuilder;
+use Minds\Core\Media\Video\CloudflareStreams\Services\PruneVideosService;
 use Minds\Core\Storage\Quotas\Manager as StorageQuotasManager;
 
 class Provider extends DiProvider
@@ -19,6 +21,14 @@ class Provider extends DiProvider
         $this->di->bind('Media\Video\CloudflareStreams\Webhooks', function (Di $di) {
             return new Webhooks(
                 storageQuotasManager: $di->get(StorageQuotasManager::class)
+            );
+        });
+
+        $this->di->bind(PruneVideosService::class, function (Di $di) {
+            return new PruneVideosService(
+                cfClient: new Client(),
+                logger: $di->get('Logger'),
+                entitiesBuilder: $di->get(EntitiesBuilder::class),
             );
         });
     }
