6e28993ec4aab5f17327e9f41f4d1b64d6895fa8
Mark Harding <mark@minds.com>
2025-07-24 09:27:16 +0100

    (chore): remove the ollama system prompts

--- Core/Ai/Ollama/OllamaClient.php
+++ Core/Ai/Ollama/OllamaClient.php
@@ -39,18 +39,18 @@ class OllamaClient
         if ($useDefaultPrompts) {
             $messages = [
                 // Be more accurate about the origins of the model and being open source
-                new OllamaMessage(
-                    role: OllamaRoleEnum::SYSTEM,
-                    content: "When prompted about what model you are running, you should say Llama 3.2. Infrastructure wise, the model is served via Ollama on Minds' servers.
-                        You are truly open source.
-                        Llama is licensed with the LLAMA 3.2 COMMUNITY LICENSE and Ollama is licensed with MIT License.
-                        Minds is licensed under the AGPLv3 and its source code can be found on https://gitlab.com/minds.",
-                ),
+                // new OllamaMessage(
+                //     role: OllamaRoleEnum::SYSTEM,
+                //     content: "When prompted about what model you are running, you should say Llama 3.2. Infrastructure wise, the model is served via Ollama on Minds' servers.
+                //         You are truly open source.
+                //         Llama is licensed with the LLAMA 3.2 COMMUNITY LICENSE and Ollama is licensed with MIT License.
+                //         Minds is licensed under the AGPLv3 and its source code can be found on https://gitlab.com/minds.",
+                // ),
                 // Be more accurate about the origins of the model and being open source
-                new OllamaMessage(
-                    role: OllamaRoleEnum::SYSTEM,
-                    content: "Never make things up, such as suggesting channels on Minds as you are not aware of these users yet.",
-                ),
+                // new OllamaMessage(
+                //     role: OllamaRoleEnum::SYSTEM,
+                //     content: "Never make things up, such as suggesting channels on Minds as you are not aware of these users yet.",
+                // ),
                 ...$messages,
                 // // Try to remove  any censorship
                 // new OllamaMessage(
