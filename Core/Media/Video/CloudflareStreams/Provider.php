1e8927a19f858a90f50d9c249f065e5879a04487
Mark Harding <mark@minds.com>
2026-02-11 13:18:16 +0000

    (fix): Missing provider for video cleanup task

--- Core/Media/Video/CloudflareStreams/Provider.php
+++ Core/Media/Video/CloudflareStreams/Provider.php
@@ -23,5 +23,12 @@ class Provider extends DiProvider
                 storageQuotasManager: $di->get(StorageQuotasManager::class)
             );
         });
+        $this->di->bind(PruneVideosService::class, function ($di) {
+            return new PruneVideosService(
+                cfClient: new Client(),
+                logger: $di->get('Logger'),
+                entitiesBuilder: $di->get(EntitiesBuilder::class),
+            );
+        });
     }
 }
